2fa3c8e712a8b462c5d72db51fc20ca39e89ab37
Aleem Tariq <aleemtariq19@gmail.com>
2021-06-02 14:55:42 -0700

    Fixed navbar and removed extra image.

--- internify/src/components/NavbarMain.js
+++ internify/src/components/NavbarMain.js
@@ -3,13 +3,19 @@ import React from 'react'
 const NavBar_Main = () => {
     return (
         <div className="navbar_home_guest">
-            <ul className="home_guest_list">
-                <li> About us </li>
-                <li> Our mission </li>
-                <li> Contact us </li>
-                <img src="../react_assets/Home(guest)/Internify.png" alt="Unable to display" className="navbar_home_guest_logo" />
-                <li> Sign in </li>
-            </ul>
+
+            <div className="navbar_home_guest_left">
+                <ul className="home_guest_list">
+                    <li> About us </li>
+                    <li> Our mission </li>
+                    <li> Contact us </li>
+                </ul>
+            </div>
+
+            <p className="internify-logo">Internify</p>
+
+            <a href="/" className="sign_in">Sign In</a> 
+            
         </div>
     )
 }
