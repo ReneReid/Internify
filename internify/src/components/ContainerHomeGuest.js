d3055b51e586731c9140130d1dcf6602c82ce7f2
Aleem Tariq <aleemtariq19@gmail.com>
2021-06-02 14:12:18 -0700

    Added schema of container home guest.

--- internify/src/components/ContainerHomeGuest.js
+++ internify/src/components/ContainerHomeGuest.js
@@ -3,19 +3,26 @@ import React from 'react'
 const Container_Home_Guest = () => {
     return (
         <div className="container_home_guest">
+
             <div className="container_one">
+
                 <div className="container_one_left">
                     <p>
                         Make job postings with gradual feedback that help guide you to make better job postings
                     </p>
                 </div>
+
                 <div className="container_one_right">
                     <img src="../react_assets/Home(guest)/create-mobile.png" alt="Unable to display" />
                 </div>
+
                 <p>
                     Progressional feedback that helps you add the right details.
                 </p>
+                
             </div>
+
+
         </div>
     )
 }
