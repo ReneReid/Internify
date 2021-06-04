1b295368400898fc5217c3398b1271b5097ef951
Aleem Tariq <aleemtariq19@gmail.com>
2021-06-04 10:57:31 -0700

    Finished navbar and header styling.

--- internify/src/components/organisms/HeaderHomeGuest.js
+++ internify/src/components/organisms/HeaderHomeGuest.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Button from '../atoms/Button.js'
 import "./styles/HeaderHomeGuest.css"
+import mail from "../../assets/Landing/email.png"
+import lock from "../../assets/Landing/lock.png"
+import google from "../../assets/Landing/google.png"
+import line from "../../assets/Landing/line.png"
 
 const header_home = () => {
 
@@ -20,8 +24,8 @@ const header_home = () => {
         <div className="header_home_guest">
             <div className="left_home_guest">
                 <div className="app_slogan">
-                    <h1 className="slogan1">Find better candidates</h1>
-                    <h1 className="slogan2">with better job postings 🥇</h1>
+                    <p className="slogan1">Find better candidates</p>
+                    <p className="slogan2">with better job postings 🥇</p>
                 </div>
                 <Button
                     className='learn_more' 
@@ -29,13 +33,18 @@ const header_home = () => {
                     onClick={learnMore}
                 />
             </div>
+
+            <div class="vl"></div>
+
             <div className="right_home_guest">
                 <Button
                     className='continue_with_Google' 
                     text='Continue with Google'
                     onClick={continueWGoogle}
-
                 />
+                <div className="linebreak">
+                    <p>─────────────── or ───────────────</p>
+                </div>
                 <form>
                     <input className="fEmail" type="text" id="fEmail" name="fEmail" placeholder="Create an account with Email"></input><br></br>
                     <input className="fPassword" type="text" id="fPassword" name="fPassword" placeholder="Password"></input><br></br>
