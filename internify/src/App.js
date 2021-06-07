433fe147ad310493d2f06b2b344fd6593cc72ae7
Jude Shamsi <judeshamsi@Judes-MacBook-Air.local>
2021-06-06 20:41:36 -0700

    added TextFieldTemp to DOM

--- internify/src/App.js
+++ internify/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import ContinueBtn from './components/atoms/ContinueBtn';
 import CheckboxTemp from './components/atoms/CheckboxTemp';
 import RadioTemp from './components/atoms/RadioTemp';
+import TextFieldTemp from './components/atoms/TextFieldTemp';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <ContinueBtn />
         <CheckboxTemp name={"posCheck"} color={"primary"} label={"Position"}/>
         <RadioTemp value={"4 months"} label={"4 months"}/>
+        <TextFieldTemp id={"input-job-title"} label={"Job Title"} defaultValue={"Front-End Developer"}/>
     </div>
   );
 }
