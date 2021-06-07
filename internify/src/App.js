79f36204c022c11f17a3724c0dc3b1d81badaca7
Jude Shamsi <judeshamsi@Judes-MacBook-Air.local>
2021-06-06 20:33:58 -0700

    added atoms to DOM

--- internify/src/App.js
+++ internify/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import ContinueBtn from './components/atoms/ContinueBtn';
+import CheckboxTemp from './components/atoms/CheckboxTemp';
+import RadioTemp from './components/atoms/RadioTemp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+        <ContinueBtn />
+        <CheckboxTemp name={"posCheck"} color={"primary"} label={"Position"}/>
+        <RadioTemp value={"4 months"} label={"4 months"}/>
     </div>
   );
 }
