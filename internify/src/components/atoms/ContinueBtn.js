4b6d8285b0ee1fc0da84bdabef63e216ec26c6b6
Jude Shamsi <judeshamsi@Judes-MacBook-Air.local>
2021-06-07 18:01:10 -0700

    added ContinueBtn css and updated btn css

--- internify/src/components/atoms/ContinueBtn.js
+++ internify/src/components/atoms/ContinueBtn.js
@@ -1,8 +1,11 @@
 import { Button } from "@material-ui/core";
+import './styles/ContinueBtn.css';
 
 function ContinueBtn() {
     return (
-        <Button>Continue</Button>
+        <div className="ContinueBtn">
+        <Button className="primary" color="secondary">Continue</Button>
+        </div>
     )
 }
 
