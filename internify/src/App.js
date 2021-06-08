b8406bb72c198904b7e4653b29294b33e4d5aa4a
Jude Shamsi <judeshamsi@Judes-MacBook-Air.local>
2021-06-07 19:55:00 -0700

    removed unused imports from app.js

--- internify/src/App.js
+++ internify/src/App.js
@@ -1,10 +1,4 @@
 import './App.css';
-import ContinueBtn from './components/atoms/ContinueBtn';
-import CheckboxTemp from './components/atoms/CheckboxTemp';
-import RadioTemp from './components/atoms/RadioTemp';
-import InputFormJobHeader from './components/molecules/InputFormJobHeader';
-import PositionSubHeader from './components/molecules/PositionSubHeader';
-import LengthSubHeader from './components/molecules/LengthSubHeader';
 import CreateJobPosting from './components/pages/CreateJobPosting';
 
 function App() {
