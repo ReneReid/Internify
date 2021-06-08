47642a20674e4bffbf40e8829edfc8f6f7a298d1
Jude Shamsi <judeshamsi@Judes-MacBook-Air.local>
2021-06-07 20:36:24 -0700

    refactored LengthSubHeader to fix radio button selection state issue

--- internify/src/components/molecules/LengthSubHeader.js
+++ internify/src/components/molecules/LengthSubHeader.js
@@ -1,14 +1,11 @@
-import RadioTemp from '../atoms/RadioTemp';
+import RadioButtonsGroup from './RadioButtonsGroup';
 
 function LengthSubHeader() {
     return(
         <div>
             <div>
             <h3>Length</h3>
-            <RadioTemp value={"4 months"} color={"primary"} label={"4 months"}/>
-            <RadioTemp value={"8 months"} color={"primary"} label={"8 months"}/>
-            <RadioTemp value={"12 months"} color={"primary"} label={"12 months"}/>
-            <RadioTemp value={"24 months"} color={"primary"} label={"24 months"}/>
+            <RadioButtonsGroup />
             </div>
         </div>
     )
