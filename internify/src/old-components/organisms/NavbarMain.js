87836dd4427929ef0ce4cfde9d1329ec65749053
Aleem Tariq <aleemtariq19@gmail.com>
2021-06-09 15:45:34 -0700

    Additional refactoring of landing page.

--- internify/src/old-components/organisms/NavbarMain.js
+++ internify/src/old-components/organisms/NavbarMain.js
@@ -1,31 +1,40 @@
-import React from 'react'
+import React from "react";
 import "./styles/NavBarMain.css";
 
 const NavBar_Main = () => {
-    return (
-        <div className="navbar_home_guest">
+  return (
+    <div className="navbar_home_guest">
+      <div className="navbar_home_guest_left">
+        <ul className="home_guest_list">
+          <li className="navbar_main_item">
+            <a href="thing1" className="navbar_main_link1">
+              About us
+            </a>
+          </li>
+          <li className="navbar_main_item">
+            <a href="thing2" className="navbar_main_link2">
+              Our mission
+            </a>
+          </li>
+          <li className="navbar_main_item">
+            <a href="thing3" className="navbar_main_link3">
+              Contact us
+            </a>
+          </li>
+        </ul>
+      </div>
 
-            <div className="navbar_home_guest_left">
-                <ul className="home_guest_list">
-                    <li className="navbar_main_item">
-                        <a href="thing1" className="navbar_main_link1">About us</a></li>
-                    <li className="navbar_main_item"> 
-                        <a href="thing2" className="navbar_main_link2">Our mission</a></li>
-                    <li className="navbar_main_item">
-                        <a href="thing3" className="navbar_main_link3">Contact us</a></li>
-                </ul>
-            </div>
+      <div className="navbar_home_guest_centre">
+        <p className="internify_logo">Internify</p>
+      </div>
 
-            <div className="navbar_home_guest_centre">
-                <p className="internify-logo">Internify</p>
-            </div>
+      <div className="navbar_home_guest_right">
+        <p className="sign_in">
+          <a href="/">Sign In</a>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-            <div className="navbar_home_guest_right">
-                <p className="sign_in"><a href="/">Sign In</a></p>
-            </div>
-        
-        </div>
-    )
-}
-
-export default NavBar_Main
+export default NavBar_Main;
