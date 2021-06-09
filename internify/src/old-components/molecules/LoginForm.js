0f7a6a0c7fee7f2c5cfa16220711492cf789e924
Tommy Tho <tommythowm@gmail.com>
2021-06-08 23:43:46 -0700

    fixed login form styles

--- internify/src/old-components/molecules/LoginForm.js
+++ internify/src/old-components/molecules/LoginForm.js
@@ -1,57 +1,66 @@
-import React from 'react'
-import Button from '../atoms/Button'
-import './styles/LBodyUpr.css'
-import '../atoms/styles/Button.css'
+import React from "react";
+import Button from "../atoms/Button";
+import "./styles/LoginForm.css";
+import "../atoms/styles/Button.css";
 
 const LoginForm = () => {
+  const continueWGoogle = () => {
+    console.log("continue with google");
+  };
 
-    const continueWGoogle = () => {
-        console.log('continue with google')
-    }
+  const login = () => {
+    console.log("login");
+  };
 
-    const login = () =>  {
-        console.log('login')
-    }
-
-// instead of using empty divs, use margins
-    return (
-        <div className='login_body_upper'>
-            {/* <div className='l_body_upr_left'></div>  */}
-            <div className='l_body_upr_cntr'>
-                <div className='welcome_message'>
-                    <p className='welcome_text'>Welcome back</p>
-                </div>
-                <div className='fields_buttons'>
-                    <Button
-                        className='continue_with_Google' 
-                        text='Continue with Google'
-                        onClick={continueWGoogle}
-                    />
-                    <div className="linebreak">
-                        <p>────────── or ──────────</p>
-                    </div>
-                    <form>
-                        <input className="fEmail" type="text" id="fEmail" name="fEmail" placeholder="Email"></input><br></br>
-                        <input className="fPassword" type="text" id="fPassword" name="fPassword" placeholder="Password"></input><br></br>
-                    </form>
-                    <Button
-                        className='login_button' 
-                        text='Login'
-                        onClick={login} />
-
-                </div>
-                <div className='create_forgot'>
-                    <div className='create_account'>
-                        <a href="placeholder1" className="create_account_link">Create an account</a>
-                    </div>
-                    <div className='forgot_password'>
-                        <a href="placeholder2" className="forgot_password_link">Forgot your password?</a>
-                    </div>
-                </div>
-            </div>
-            {/* <div className='l_body_upr_right'></div>       */}
+  // instead of using empty divs, use margins
+  return (
+    <div className="login-container">
+      <div className="welcome_message">
+        <p className="welcome_text">Welcome back 👋</p>
+      </div>
+      <div className="fields_buttons">
+        <Button
+          className="continue_with_Google"
+          text="Continue with Google"
+          onClick={continueWGoogle}
+        />
+        <div className="linebreak">
+          <p>────────── or ──────────</p>
         </div>
-    )
-}
+        <form>
+          <input
+            className="fEmail"
+            type="text"
+            id="fEmail"
+            name="fEmail"
+            placeholder="Email"
+          />
+          <br></br>
+          <input
+            className="fPassword"
+            type="text"
+            id="fPassword"
+            name="fPassword"
+            placeholder="Password"
+          ></input>
+          <br></br>
+        </form>
+        <Button className="login_button" text="Login" onClick={login} />
+        <div className="create_forgot">
+        <div className="create_account">
+          <a href="placeholder1" className="create_account_link">
+            Create an account
+          </a>
+        </div>
+        <div className="forgot_password">
+          <a href="placeholder2" className="forgot_password_link">
+            Forgot your password?
+          </a>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+};
 
-export default LoginForm
+export default LoginForm;
