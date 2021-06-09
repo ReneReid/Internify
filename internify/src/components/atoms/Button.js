1e14442918576b4ddb4824b9d4b14b24d0fdb8fc
Tommy Tho <tommythowm@gmail.com>
2021-06-09 00:15:59 -0700

    added white button for login and landing page

--- internify/src/components/atoms/Button.js
+++ internify/src/components/atoms/Button.js
@@ -48,3 +48,19 @@ export const ButtonClear = withStyles(() => ({
         },
       }
 }))(Button);
+
+export const ButtonWhite = withStyles(() => ({
+  root: {
+    textTransform: "none",
+    fontWeight: 700,
+    padding: "0.5em 1em",
+    backgroundColor: "white",
+    color: "black",
+    borderRadius: "5px",
+    "&:hover": {
+      backgroundColor: "white",
+      color: "black",
+      boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.4)",
+    },
+  }
+}))(Button);
