69447fd2d242132181854b6210e6c63825526d4a
Tommy Tho <tommythowm@gmail.com>
2021-06-09 00:02:36 -0700

    fixed navbar

--- internify/src/old-components/organisms/LoginBody.js
+++ internify/src/old-components/organisms/LoginBody.js
@@ -1,21 +1,15 @@
-import React from 'react';
-import LoginForm from '../molecules/LoginForm';
-import Footer from '../molecules/Footer';
-import './styles/LoginBody.css'
-
+import React from "react";
+import LoginForm from "../molecules/LoginForm";
+import Footer from "../molecules/Footer";
+import "./styles/LoginBody.css";
 
 const LoginBody = () => {
-    return (
-        <div className='login_body'>
-            {/* rename this to "Login Form" */}
-            <LoginForm />
-            
-            {/* rename LBodyLwr to "Footer" */}
-            <div className='login_body_lower'>
-                <Footer />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="login_body">
+      <LoginForm />
+      <Footer />
+    </div>
+  );
+};
 
-export default LoginBody
+export default LoginBody;
