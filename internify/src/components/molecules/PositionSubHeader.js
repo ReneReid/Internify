e5fb953710781c4d9ceaacf7f5dc32cdd7dde38f
Jude Shamsi <judeshamsi@Judes-MacBook-Air.local>
2021-06-08 21:47:44 -0700

    removed passing in color via props for radio buttons and check boxes

--- internify/src/components/molecules/PositionSubHeader.js
+++ internify/src/components/molecules/PositionSubHeader.js
@@ -1,15 +1,15 @@
 
 import CheckboxTemp from '../atoms/CheckboxTemp';
 
-function PositionSubHeader(props) {
+function PositionSubHeader() {
     return(
         <div>
             <div>
             <h3>Position</h3>
-            <CheckboxTemp name={"Internship"} color={"primary"} label={"Internship"}/>
-            <CheckboxTemp name={"Co-op"} color={"primary"} label={"Co-op"}/>
-            <CheckboxTemp name={"New Grad"} color={"primary"} label={"New Grad"}/>
-            <CheckboxTemp name={"Part-time"} color={"primary"} label={"Part-time"}/>
+            <CheckboxTemp name={"Internship"} label={"Internship"}/>
+            <CheckboxTemp name={"Co-op"} label={"Co-op"}/>
+            <CheckboxTemp name={"New Grad"} label={"New Grad"}/>
+            <CheckboxTemp name={"Part-time"} label={"Part-time"}/>
             </div>
         </div>
     )
--- internify/src/components/molecules/RadioButtonsGroup.js
+++ internify/src/components/molecules/RadioButtonsGroup.js
@@ -14,10 +14,10 @@ function RadioButtonsGroup() {
     <FormControl component="fieldset">
       <RadioGroup aria-label="position-length" name="pos1" value={value} onChange={handleChange}>
         <div>
-        <RadioTemp value={"4 months"} color={"primary"} label={"4 months"}/>
-        <RadioTemp value={"8 months"} color={"primary"} label={"8 months"}/>
-        <RadioTemp value={"12 months"} color={"primary"} label={"12 months"}/>
-        <RadioTemp value={"24 months"} color={"primary"} label={"24 months"}/>
+        <RadioTemp value={"4 months"} label={"4 months"}/>
+        <RadioTemp value={"8 months"}  label={"8 months"}/>
+        <RadioTemp value={"12 months"}  label={"12 months"}/>
+        <RadioTemp value={"24 months"} label={"24 months"}/>
         </div>
       </RadioGroup>
     </FormControl>
