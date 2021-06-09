2cd68a0bd716e6acb52006cb9ad7ab4ca4a0541d
Jude Shamsi <judeshamsi@Judes-MacBook-Air.local>
2021-06-08 21:54:12 -0700

    updated radiobtn group to use new name

--- internify/src/components/molecules/RadioButtonsGroup.js
+++ internify/src/components/molecules/RadioButtonsGroup.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import RadioGroup from '@material-ui/core/RadioGroup';
-import RadioTemp from '../atoms/RadioTemp';
+import RadioBtn from '../atoms/RadioBtn';
 import FormControl from '@material-ui/core/FormControl';
 
 function RadioButtonsGroup() {
@@ -14,10 +14,10 @@ function RadioButtonsGroup() {
     <FormControl component="fieldset">
       <RadioGroup aria-label="position-length" name="pos1" value={value} onChange={handleChange}>
         <div>
-        <RadioTemp value={"4 months"} label={"4 months"}/>
-        <RadioTemp value={"8 months"}  label={"8 months"}/>
-        <RadioTemp value={"12 months"}  label={"12 months"}/>
-        <RadioTemp value={"24 months"} label={"24 months"}/>
+        <RadioBtn value={"4 months"} label={"4 months"}/>
+        <RadioBtn value={"8 months"}  label={"8 months"}/>
+        <RadioBtn value={"12 months"}  label={"12 months"}/>
+        <RadioBtn value={"24 months"} label={"24 months"}/>
         </div>
       </RadioGroup>
     </FormControl>
