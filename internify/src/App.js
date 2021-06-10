e0ffb6586f986b2a599d5979a7654e519053eade
Tommy <37882255+tommytwm@users.noreply.github.com>
2021-06-09 21:08:35 -0700

    [11A, 11D, 11E, 11F] - Atomic Components (#9)
    * material ui init
    
    * added button-filled
    
    * added outlined and clear buttons
    
    * added stars
    
    * added plus icon
    
    * added active state to icons
    
    * added basic table
    
    * added the other table
    
    * added chips
    
    * cleared app.js
    
    * added white button for login and landing page
    
    * [User Story 11F] - Merge Job Posting atoms  (#10)
    
    * added CheckBox atom
    
    * added RadioBtn atom
    
    * added TextFieldInput atom
    
    Co-authored-by: Jude Shamsi <judeshamsi@Judes-MacBook-Air.local>
    
    Co-authored-by: Jude Shamsi <48394562+JudeShamsi@users.noreply.github.com>
    Co-authored-by: Jude Shamsi <judeshamsi@Judes-MacBook-Air.local>

--- internify/src/App.js
+++ internify/src/App.js
@@ -1,5 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  ButtonFilled,
+  ButtonOutlined,
+  ButtonClear,
+} from "./components/atoms/Button";
+import { StarPlain, StarColoured, Add } from "./components/atoms/Icon";
+import { ChipBasic, ChipClickable } from "./components/atoms/Chips";
+import { TableBasic, TableStar } from "./components/molecules/Table";
+
+const data = [
+  {
+    isStarred: false,
+    title: "Software Developer",
+    dateCreated: new Date("05/25/2021").toLocaleDateString(),
+    score: "Good",
+  },
+];
 
 function App() {
   return (
