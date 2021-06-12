0150fe740b7649192244be9b54b9231e9d1c501d
Tommy Tho <tommythowm@gmail.com>
2021-06-11 21:43:51 -0700

    removed comments on app.js

--- internify/src/App.js
+++ internify/src/App.js
@@ -1,15 +1,6 @@
 import "./App.css";
 import Create from './components/pages/Create';
 
-// const data = [
-//   {
-//     isStarred: false,
-//     title: "Software Developer",
-//     dateCreated: new Date("05/25/2021").toLocaleDateString(),
-//     score: "Good",
-//   },
-// ];
-
 function App() {
   return (
     <div className="App">
