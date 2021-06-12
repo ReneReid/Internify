707c0b1517531ab1479f6b7401135da84fd78242
Jude Shamsi <judeshamsi@Judes-MacBook-Air.local>
2021-06-11 17:05:22 -0700

    updated css for navbar links

--- internify/src/old-components/organisms/NavbarMain.js
+++ internify/src/old-components/organisms/NavbarMain.js
@@ -7,17 +7,17 @@ const NavBar_Main = () => {
       <div className="navbar_home_guest_left">
         <ul className="home_guest_list">
           <li className="navbar_main_item">
-            <a href="thing1" className="navbar_main_link1">
+            <a href="thing1" className="navbar_main_link">
               About us
             </a>
           </li>
           <li className="navbar_main_item">
-            <a href="thing2" className="navbar_main_link2">
+            <a href="thing2" className="navbar_main_link">
               Our mission
             </a>
           </li>
           <li className="navbar_main_item">
-            <a href="thing3" className="navbar_main_link3">
+            <a href="thing3" className="navbar_main_link">
               Contact us
             </a>
           </li>
