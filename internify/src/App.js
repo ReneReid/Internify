3df02b784ae332857e449c5ddb474d32dc0b53c6
Jude Shamsi <judeshamsi@Judes-MacBook-Air.local>
2021-06-11 22:08:11 -0700

    removed home routing path from app.js

--- internify/src/App.js
+++ internify/src/App.js
@@ -11,9 +11,6 @@ function App() {
   return (
     <Router>
        <Switch>
-          <Route path="/home">
-            <Landing />
-          </Route>
           <Route path="/login">
             <Login />
           </Route>
