a81673353e921c6aad237068460ec6e87e734b0d
Jude Shamsi <jude.shamsi@gmail.com>
2021-06-11 22:43:46 -0700

    added all components to app.js for routing

--- internify/src/App.js
+++ internify/src/App.js
@@ -1,5 +1,8 @@
 import Landing from "./old-components/pages/Landing";
 import Login from "./old-components/pages/Login";
+import CreateJobHeader from "./components/pages/CreateJobPosting/CreateJobHeader";
+import TechRequirements from "./components/pages/CreateJobPosting/TechRequirements";
+import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,21 +10,35 @@ import {
   Link
 } from "react-router-dom";
 
+const mockTechStackData = {
+  "languages": ["Java", "JavaScript", "C++", "C"],
+  "frameworks": ["React", "Angular", "HTML", "CSS"],
+  "workTools": ["Jira", "Asana", "Confluence", "Notion"],
+  "csConcepts": ["Object Oriented Programming", "Functional Programming", "Recursion"],
+};
+
 function App() {
   return (
-    <Router>
+    <div className="App">
+      <header className="App-header">
+      <Router>
        <Switch>
           <Route path="/login">
             <Login />
           </Route>
-          <Route path="/create">
-           <h2>Hello from create page</h2>
+          <Route path="/create1">
+           <CreateJobHeader />
+          </Route>
+          <Route path="/create2">
+           <TechRequirements data={mockTechStackData}/>
           </Route>
           <Route path="/">
             <Landing />
           </Route>
         </Switch>
     </Router>
+      </header>
+    </div>
   );
 }
 
