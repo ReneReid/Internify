42befb131f3810b22b844dd45667febef02b2328
Jude Shamsi <48394562+JudeShamsi@users.noreply.github.com>
2021-06-11 22:45:26 -0700

    [User Story 13A] - Embedded React Routing  (#14)
    * added react-router-dom to package.json
    
    * added initial routing to App.js
    
    * added landing page to routing
    
    * removed home routing path from app.js
    
    * added all components to app.js for routing
    
    Co-authored-by: Jude Shamsi <judeshamsi@Judes-MacBook-Air.local>

--- internify/src/App.js
+++ internify/src/App.js
@@ -1,10 +1,44 @@
 import Landing from "./old-components/pages/Landing";
 import Login from "./old-components/pages/Login";
-import "./App.css";
+import CreateJobHeader from "./components/pages/CreateJobPosting/CreateJobHeader";
+import TechRequirements from "./components/pages/CreateJobPosting/TechRequirements";
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const mockTechStackData = {
+  "languages": ["Java", "JavaScript", "C++", "C"],
+  "frameworks": ["React", "Angular", "HTML", "CSS"],
+  "workTools": ["Jira", "Asana", "Confluence", "Notion"],
+  "csConcepts": ["Object Oriented Programming", "Functional Programming", "Recursion"],
+};
 
 function App() {
   return (
-    <Login/>
+    <div className="App">
+      <header className="App-header">
+      <Router>
+       <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/create1">
+           <CreateJobHeader />
+          </Route>
+          <Route path="/create2">
+           <TechRequirements data={mockTechStackData}/>
+          </Route>
+          <Route path="/">
+            <Landing />
+          </Route>
+        </Switch>
+    </Router>
+      </header>
+    </div>
   );
 }
 
