8793738588a2b6f4844d67c587e64b93039c4b5d
Tommy Tho <tommythowm@gmail.com>
2021-06-15 23:26:26 -0700

    cleaned footer classnames

--- internify/src/components/organisms/Footer.js
+++ internify/src/components/organisms/Footer.js
@@ -1,26 +1,33 @@
-import React from 'react'
-import "./styles/Footer.css"
-
+import React from "react";
+import "./styles/Footer.css";
 
 const Footer = () => {
-    return (
-        <div className="footer_home_guest">
-            <div className="footer_home_guest_left">
-                <ul className="home_guest_list_footer">
-                    <li className="footer_item">
-                        <a href="thing1" className="navbar_main_link1">About us</a></li>
-                    <li className="footer_item"> 
-                        <a href="thing2" className="navbar_main_link2">Our mission</a></li>
-                    <li className="footer_item">
-                        <a href="thing3" className="navbar_main_link3">Contact us</a></li>
-                </ul>
-            </div>
-            <div className="footer_homeguest_right">
-                <h4 className='appNameFooter'>Internify</h4>
-            </div>
-
-        </div>
-    )
-}
+  return (
+    <div className="footer">
+      <div className="footer_links_container">
+        <ul className="footer_links">
+          <li className="footer_item">
+            <a href="thing1" className="navbar_main_link1">
+              About us
+            </a>
+          </li>
+          <li className="footer_item">
+            <a href="thing2" className="navbar_main_link2">
+              Our mission
+            </a>
+          </li>
+          <li className="footer_item">
+            <a href="thing3" className="navbar_main_link3">
+              Contact us
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer_logo_container">
+        <h4 className="footer_logo">Internify</h4>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
