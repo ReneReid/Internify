6179f98249410fd842e77013527dd77bc56cd3d7
Tommy Tho <tommythowm@gmail.com>
2021-06-16 12:32:24 -0700

    fixed navbar so that logo is centred

--- internify/src/components/organisms/Navbar.js
+++ internify/src/components/organisms/Navbar.js
@@ -4,33 +4,31 @@ import "./styles/Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="navbar_home_guest">
-      <div className="navbar_home_guest_left">
-        <ul className="home_guest_list">
-          <li className="navbar_main_item">
-            <a href="thing1" className="navbar_main_link">
-              About us
-            </a>
-          </li>
-          <li className="navbar_main_item">
-            <a href="thing2" className="navbar_main_link">
-              Our mission
-            </a>
-          </li>
-          <li className="navbar_main_item">
-            <a href="thing3" className="navbar_main_link">
-              Contact us
-            </a>
-          </li>
-        </ul>
-      </div>
+    <div className="navbar_container">
+      <ul className="home_guest_links">
+        <li className="navbar_main_item">
+          <a href="thing1" className="navbar_main_link">
+            About us
+          </a>
+        </li>
+        <li className="navbar_main_item">
+          <a href="thing2" className="navbar_main_link">
+            Our mission
+          </a>
+        </li>
+        <li className="navbar_main_item">
+          <a href="thing3" className="navbar_main_link">
+            Contact us
+          </a>
+        </li>
+      </ul>
 
       <Link className="navbar_logo" to="/">
-        <p className="navbar_internify">Internify</p>
+        Internify
       </Link>
 
       <Link className="navbar_login" to="/login">
-          <p>Sign In</p>
+        Sign In
       </Link>
     </div>
   );
