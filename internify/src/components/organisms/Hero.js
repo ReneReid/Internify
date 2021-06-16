c3ecdfba617a41c887c47fea99a46d74cc9424c0
Tommy Tho <tommythowm@gmail.com>
2021-06-16 00:04:54 -0700

    cleaned hero banner css:

--- internify/src/components/organisms/Hero.js
+++ internify/src/components/organisms/Hero.js
@@ -1,56 +1,62 @@
-import React from 'react'
-import { ButtonWhite } from '../atoms/index.js'
-import "./styles/Hero.css"
+import React from "react";
+import { ButtonWhite } from "../atoms/index.js";
+import "./styles/Hero.css";
 
 const Hero = () => {
+  const learnMore = () => {
+    console.log("learn more");
+  };
 
-    const learnMore = () => {
-        console.log('learn more')
-    }
+  const continueWGoogle = () => {
+    console.log("continue with google");
+  };
 
-    const continueWGoogle = () => {
-        console.log('continue with google')
-    }
+  const continueWEmail = () => {
+    console.log("continue with email");
+  };
 
-    const continueWEmail = () =>  {
-        console.log('continue with email')
-    }
-
-    return (
-        <div className="header_home_guest">
-            <div className="left_home_guest">
-                <div className="app_slogan">
-                    <p className="slogan1">Find better candidates</p>
-                    <p className="slogan2">with better job postings 🥇</p>
-                </div>
-                <ButtonWhite
-                    className='learn_more' 
-                    onClick={learnMore}
-                >Learn More</ButtonWhite>
-            </div>
-
-            {/* <div class="vl"></div> */}
-
-            <div className="right_home_guest">
-                <ButtonWhite
-                    className='continue_with_Google' 
-                    onClick={continueWGoogle}
-                >Continue with Google</ButtonWhite>
-                <div className="linebreak">
-                    <p>────────── or ──────────</p>
-                </div>
-                <form>
-                    <input className="fEmail" type="text" id="fEmail" name="fEmail" placeholder="Create an account with Email"></input><br></br>
-                    <input className="fPassword" type="text" id="fPassword" name="fPassword" placeholder="Password"></input><br></br>
-                </form>
-                <ButtonWhite
-                    className='continue_with_email' 
-                    onClick={continueWEmail}
-                >Continue with Email</ButtonWhite>
-            </div>
- 
+  return (
+    <div className="hero">
+      <div className="hero_header_container">
+        <div className="hero_header_content">
+          <p className="hero_header_line1">Find better candidates</p>
+          <p className="hero_header_line2">with better job postings 🥇</p>
         </div>
-    )
-}
+        <ButtonWhite className="learn_more" onClick={learnMore}>
+          Learn More
+        </ButtonWhite>
+      </div>
+      <div className="hero_form_container">
+        <ButtonWhite className="hero_form_button" onClick={continueWGoogle}>
+          Continue with Google
+        </ButtonWhite>
+        <div className="linebreak">
+          <p>────────── or ──────────</p>
+        </div>
+        <form>
+          <input
+            className="hero_form_input"
+            type="text"
+            id="hero_email"
+            name="fEmail"
+            placeholder="Create an account with Email"
+          />
+          <br></br>
+          <input
+            className="hero_form_input"
+            type="text"
+            id="hero_password"
+            name="fPassword"
+            placeholder="Password"
+          />
+          <br></br>
+        </form>
+        <ButtonWhite className="hero_form_button" onClick={continueWEmail}>
+          Continue with Email
+        </ButtonWhite>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
