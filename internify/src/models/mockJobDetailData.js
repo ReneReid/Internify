5a45c2893e69288bc2ce39fa7ad5aaf598b22fc8
Jude Shamsi <jude.shamsi@gmail.com>
2021-06-18 14:39:33 -0700

    created models directory and placed mock data for Job Details page

---
+++ internify/src/models/mockJobDetailData.js
@@ -0,0 +1,10 @@
+
+const mockJobDetailData = {
+    "location": ["In-person", "Remote", "Hybrid"],
+    "payment": ["Hourly", "Salary", "Hourly + Commission"],
+    "citizenship": ["Citizens only", "Citizens & PR", "Citizens, PR, & International Students", "Anyone"],
+    "academicReq": ["Associate's Degree", "Bachelor's Degree", "Master's Degree", "Ph.D", "Diploma", "Certification"],
+    "coopReq": ["Yes", "No"]
+  };
+
+  export default mockJobDetailData;
