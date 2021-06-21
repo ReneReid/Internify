852224e6691b04bbd26b334ef473fcc1dac30f3c
Jude Shamsi <jude.shamsi@gmail.com>
2021-06-20 20:15:34 -0700

    added state functionality to PositionSubHeader that updates redux store with selected position

--- internify/src/components/molecules/LengthSubHeader.js
+++ internify/src/components/molecules/LengthSubHeader.js
@@ -1,14 +1,15 @@
 import RadioButtonsGroup from './RadioButtonsGroup';
 
-function LengthSubHeader() {
-
+function LengthSubHeader(props) {
+    const jobData = props.jobData;
     const data = ["4 months", "8 months", "12 months", "16 months", "24 months"]
+
 
     return(
         <div>
             <div>
             <h3>Length</h3>
-            <RadioButtonsGroup data={data}/>
+            <RadioButtonsGroup data={jobData}/>
             </div>
         </div>
     )
--- internify/src/components/molecules/PositionSubHeader.js
+++ internify/src/components/molecules/PositionSubHeader.js
@@ -1,15 +1,38 @@
 
 import CheckBox from '../atoms/CheckBox';
+import React from 'react';
+import { useState } from 'react';
 
-function PositionSubHeader() {
+function PositionSubHeader(props) {
+
+    const data = props.jobData;
+    const [state, setCheckedState] = useState({
+        Internship: false,
+        Coop: false,
+        NewGrad: false,
+        PartTime: false
+    })
+
+      const handleChange = (event) => {     
+          setCheckedState({ ...state, [event.target.name]: event.target.checked });
+          if(event.target.checked){
+              data.positionType.push(event.target.name);
+          } else {
+              if(data.positionType.includes(event.target.name)){
+                  data.positionType = data.positionType.filter(obj => obj !== event.target.name);
+              }
+          }
+      };
+    
+
     return(
         <div>
             <div>
             <h3>Position</h3>
-            <CheckBox name={"Internship"} label={"Internship"}/>
-            <CheckBox name={"Co-op"} label={"Co-op"}/>
-            <CheckBox name={"New Grad"} label={"New Grad"}/>
-            <CheckBox name={"Part-time"} label={"Part-time"}/>
+            <CheckBox name={"Internship"} label={"Internship"} onChange={handleChange} checked={state.Internship}/>
+            <CheckBox name={"Coop"} label={"Co-op"} onChange={handleChange} checked={state.Coop}/>
+            <CheckBox name={"NewGrad"} label={"New Grad"} onChange={handleChange} checked={state.NewGrad}/>
+            <CheckBox name={"PartTime"} label={"Part-time"} onChange={handleChange} checked={state.PartTime}/>
             </div>
         </div>
     )
