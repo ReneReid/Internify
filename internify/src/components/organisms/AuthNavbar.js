f2eab307a139da369b91e472f9ff0d04b1cb5422
Rene Reid <rdreid01@students.cs.ubc.ca>
2021-06-21 13:54:19 -0700

    horizontal line replaced with box-shadow, elements further justified left/right

--- internify/src/components/organisms/AuthNavbar.js
+++ internify/src/components/organisms/AuthNavbar.js
@@ -2,23 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./styles/AuthNavbar.css";
 import MenuIcon from "@material-ui/icons/Menu";
-import Container from "@material-ui/core/Container";
 import Grid from '@material-ui/core/Grid';
 
 
 const Navbar = () => {
   return (
-    <Container>
       <Grid container xs = {12} sm = {12} md = {12} className = "navbar_container">
 
-        <Grid item xs = {6} sm = {6} md = {6} style={{height: 70, width: '100%'}} className="logo">
+        <Grid item xs = {4} sm = {4} md = {4} style={{height: 70, width: '100%'}} className="logo" >
           <Link className = "auth_navbar_logo" to="/">
             Internify
           </Link>
-
-
         </Grid>
-        <Grid container xs = {6} sm = {6} md = {6} className="auth_navbar_right_items">
+        <Grid container xs = {8} sm = {8} md = {8} className="auth_navbar_right_items" justify='flex-end'>
           <Grid className="auth_navbar_right_item">
           <Link className="auth_navbar_item" to="placeHolder">
               Home
@@ -44,17 +40,12 @@ const Navbar = () => {
 
           <Grid className="auth_navbar_right_item">
             <Link className="auth_navbar_item" to="placeHolder">
-
                 <MenuIcon className="auth_navbar_item"></MenuIcon>
             </Link>
           </Grid>
-
         </Grid>
-
       </Grid>
-      <hr></hr> 
-      
-    </Container>
+     
   );
 };
 
