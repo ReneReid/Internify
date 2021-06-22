971be18281e1fbd40d17d0646b9d893f7768e194
Aleem Tariq <aleemtariq19@gmail.com>
2021-06-21 19:25:10 -0700

    Requested fixes for multi-line text field, as well as styling.

--- internify/src/components/molecules/InputFormContactDetails.js
+++ internify/src/components/molecules/InputFormContactDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TextFieldInput from "../atoms/TextFieldInput";
+import MultiLineTextField from '../atoms/MultiLineTextField';
 import "./styles/InputFormContactDetails.css";
 
 
@@ -48,11 +49,11 @@ function InputFormContactDetails(props){
             </div>
 
             <div className="input_contact_row">
-                <TextFieldInput 
+                <MultiLineTextField 
                     id={"header-form-other"}
                     className={"input_contact_row_i-form-alt"}
                     label={"Application steps"}
-                    defaultValue={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+                    defaultValue={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
                     type={"text"}
                 />
             </div>
--- internify/src/components/pages/CreateJobPosting/ContactDetails.js
+++ internify/src/components/pages/CreateJobPosting/ContactDetails.js
@@ -5,12 +5,11 @@ import {
     Stepper,
     InputFormContactDetails
   } from "../../molecules/index";
-import "./styles/ContactDetails.css";
 
 const ContactDetails = () => {
     return (
         <div className="contact_details">
-            <Container maxWidth="md" className={"container"}>
+            <Container maxWidth="md" className={"container"} style={{ padding: "0 10em" }}>
                 <Stepper stepNumber={3} />
                 <div className="contact_details_container">
                     <h1>4. Contact Details</h1>
