c5c22a4d2f5f5f4f35ebda56e404c7c3cbfc130b
Aleem Tariq <aleemtariq19@gmail.com>
2021-06-21 17:07:22 -0700

    Fixed style overriding issue.

--- internify/src/components/molecules/InputFormContactDetails.js
+++ internify/src/components/molecules/InputFormContactDetails.js
@@ -10,7 +10,7 @@ function InputFormContactDetails(props){
             <div className="input_contact_row">
                 <TextFieldInput 
                     id={"header-form-name"}
-                    className={"i-form-outer"}
+                    className={"input_contact_row_i-form-outer"}
                     label={"Recruiter Name"}
                     defaultValue={"Mahesh Swarani"}
                     type={"text"}
@@ -20,7 +20,7 @@ function InputFormContactDetails(props){
             <div className="input_contact_row">
                 <TextFieldInput 
                     id={"header-form-email"}
-                    className={"i-form-outer"}
+                    className={"input_contact_row_i-form-outer"}
                     label={"Recruiter Email"}
                     defaultValue={"mahesh@astragtech.ca"}
                     type={"text"}
@@ -30,7 +30,7 @@ function InputFormContactDetails(props){
             <div className="input_contact_row">
                 <TextFieldInput 
                     id={"header-form-linkedin"}
-                    className={"i-form-outer"}
+                    className={"input_contact_row_i-form-outer"}
                     label={"LinkedIn"}
                     defaultValue={"linkedin.com/maheshswaran"}
                     type={"text"}
@@ -40,7 +40,7 @@ function InputFormContactDetails(props){
             <div className="input_contact_row">
                 <TextFieldInput 
                     id={"header-form-other"}
-                    className={"i-form-outer"}
+                    className={"input_contact_row_i-form-outer"}
                     label={"Other contact"}
                     defaultValue={"+1 (601) 123 4356"}
                     type={"text"}
@@ -50,7 +50,7 @@ function InputFormContactDetails(props){
             <div className="input_contact_row">
                 <TextFieldInput 
                     id={"header-form-other"}
-                    className={"i-form-alt"}
+                    className={"input_contact_row_i-form-alt"}
                     label={"Application steps"}
                     defaultValue={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
                     type={"text"}
