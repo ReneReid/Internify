88c6c3982c731bfb7c7756fa2af2a9b736942b06
Tommy Tho <tommythowm@gmail.com>
2021-06-22 22:50:34 -0700

    added job header

--- internify/src/components/molecules/JobPosting.js
+++ internify/src/components/molecules/JobPosting.js
@@ -2,10 +2,16 @@ import React from "react";
 import "./styles/JobPosting.css";
 
 const JobPosting = ({data}) => {
-    console.log(data);
+    const header = data.header;
+    const requirements = data.requirements;
+    const details = data.details;
+    const contact = data.contact;
+
     return (
         <div className="job_posting_container">
-
+            <h3 className="job_posting_title">{header.title}</h3>
+            <h4 className="job_posting_subheader">{header.company}</h4>
+            <p className="job_posting_subheader">{header.location}</p>
         </div>
     );
 };
