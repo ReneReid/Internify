f90fd07bbd78e484a6954c0866a20df722c295db
Jude Shamsi <jude.shamsi@gmail.com>
2021-06-23 19:12:16 -0700

    updated TechStack to use handlers for each checkbox section

--- internify/src/components/molecules/TechStack.js
+++ internify/src/components/molecules/TechStack.js
@@ -9,6 +9,23 @@ const TechStack = (props) => {
   const workTools = props.workTools;
   const csConcepts = props.csConcepts;
 
+  function handleLanguageChange(event){
+    props.jobData.codingLanguages.push(event.target.name);
+  }
+
+  function handleFrameworksChange(event){
+    props.jobData.frameworks.push(event.target.name);
+  }
+
+  function handleWorkToolsChange(event){
+    props.jobData.tools.push(event.target.name);
+  }
+
+  function handleConceptsChange(event){
+    props.jobData.compSciConcepts.push(event.target.name);
+  }
+
+  
   return (
     <React.Fragment>
       <h2>Tech stack</h2>
@@ -16,7 +33,7 @@ const TechStack = (props) => {
         <h3>Coding Languages*</h3>
         <div className="tech_stack_checkbox_container">
           {languages.map((x) => {
-            return <CheckBox name={x} label={x} />;
+            return <CheckBox name={x} label={x} onChange={handleLanguageChange} />;
           })}
         </div>
         <TextField id="languages" label="Custom..." variant="filled" />
@@ -25,7 +42,7 @@ const TechStack = (props) => {
         <h3>Frameworks*</h3>
         <div className="tech_stack_checkbox_container">
           {frameworks.map((x) => {
-            return <CheckBox name={x} label={x} />;
+            return <CheckBox name={x} label={x} onChange={handleFrameworksChange} />;
           })}
         </div>
         <TextField id="languages" label="Custom..." variant="filled" />
@@ -34,7 +51,7 @@ const TechStack = (props) => {
         <h3>Work Tools and Others*</h3>
         <div className="tech_stack_checkbox_container">
           {workTools.map((x) => {
-            return <CheckBox name={x} label={x} />;
+            return <CheckBox name={x} label={x} onChange={handleWorkToolsChange} />;
           })}
         </div>
         <TextField id="languages" label="Custom..." variant="filled" />
@@ -43,7 +60,7 @@ const TechStack = (props) => {
         <h3>Computer Science Concepts</h3>
         <div className="tech_stack_checkbox_container">
           {csConcepts.map((x) => {
-            return <CheckBox name={x} label={x} />;
+            return <CheckBox name={x} label={x} onChange={handleConceptsChange} />;
           })}
         </div>
         <TextField id="languages" label="Custom..." variant="filled" />
