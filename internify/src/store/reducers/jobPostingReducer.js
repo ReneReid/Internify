4f4294b771c860c8710adc38bd0d552f08c80657
Jude Shamsi <jude.shamsi@gmail.com>
2021-06-23 19:12:55 -0700

    updated reducer to take in one currentPosting

--- internify/src/store/reducers/jobPostingReducer.js
+++ internify/src/store/reducers/jobPostingReducer.js
@@ -2,20 +2,16 @@
 import { ADD_JOB_HEADER } from "../actions/types/jobPostTypes";
 
 const initialState = {
-    jobHeaderData: [],
-    techStackData: [],
-    jobDetailsData: [],
-    contactDetailsData: [],
+    currentPosting: {},
     finalJobPostingsList: []
 }
 
 export default function(state = initialState, action) {
     switch(action.type) { 
         case ADD_JOB_HEADER:
-            initialState.jobHeaderData.push(action.payload);
             return {
                 ...state,
-                jobHeaderData: initialState.jobHeaderData
+                currentPosting: action.payload
             } 
         default:
             return state;
