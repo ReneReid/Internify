e0c14eccb1c2e2385ecac6c22264c3a83de930fd
Aleem Tariq <aleemtariq19@gmail.com>
2021-06-25 12:13:54 -0700

    Basic outline for Notes completed.

--- internify/src/components/molecules/Notes.js
+++ internify/src/components/molecules/Notes.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const Notes = () => {
+const Notes = ({canadaMatches, ubcMatches, totalMatches, totalStudents}) => {
     return (
         <div className="notes_container">
             <h3>Notes</h3>
+                <div className="notes_row">
+                    <h5>
+                        {totalMatches} out of {totalStudents} matches in our DB
+                    </h5> 
+                </div>
+                <div className="notes_row">
+                    <h5>
+                        {ubcMatches} matches with UBC students
+                    </h5>
+                </div>
+                <div className="notes_row">
+                    <h5>
+                        {canadaMatches} total matches in Canada
+                    </h5>
+                </div>
         </div>
     )
 }
