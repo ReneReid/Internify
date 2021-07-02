dd66d9568b23817baa221f8d290ceba88ee929c4
Aleem Tariq <aleemtariq19@gmail.com>
2021-07-01 22:25:07 -0700

    Need to redesign according to Grid's and sub-grids for optional spacing layout.

--- internify/src/components/pages/Profile.js
+++ internify/src/components/pages/Profile.js
@@ -5,36 +5,9 @@ import React from "react";
 
 const Profile = () => {
   return (
-    <Grid
-      container
-      spacing={12}
-      direction="row"
-      justify="center"
-      alignItems="center"
-    >
-      <Grid container item xs={5}>
-        <div className="profile_header_outer">
-          <div className="profile_header_tag">
-            <h1>Your Profile</h1>
-          </div>
-
-          <div className="profile_header_inner_1">
-            <h3> Pic and name </h3>
-          </div>
-
-          <div className="profile_header_inner_2">
-            <h3> Description </h3>
-          </div>
-
-          <div className="profile_header_inner_3">
-            <h3> Contact details </h3>
-          </div>
-        </div>
-      </Grid>
-      <Grid container item xs={5}>
-        <h1> World </h1>
-      </Grid>
-    </Grid>
+    <div>
+      <h1>Test</h1>
+    </div>
   );
 };
 export default Profile;
