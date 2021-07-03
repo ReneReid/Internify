b6117353f3763ec2d0c194b25286b5f1b8c609d6
Aleem Tariq <aleemtariq19@gmail.com>
2021-07-02 19:45:53 -0700

    Having trouble, need to review.

--- internify/src/components/pages/Profile.js
+++ internify/src/components/pages/Profile.js
@@ -4,35 +4,6 @@ import { Grid } from "@material-ui/core";
 import React from "react";
 
 const Profile = () => {
-  return (
-    <Grid container direction="column" justify="center" alignItems="center">
-      <Grid item>
-        <div className="header_tag_outer">
-          <div className="header_tag_inner_left">
-            <h2> Your Profile </h2>
-          </div>
-          <div className="header_tag_inner_right">
-            <button>Edit profile</button>
-          </div>
-        </div>
-      </Grid>
-
-      <Grid container direction="row" justify="center" alignItems="center">
-        <Grid item>
-          <h1> Hello </h1>
-        </Grid>
-        <Grid item>
-          <h1> World </h1>
-        </Grid>
-      </Grid>
-
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-      ></Grid>
-    </Grid>
-  );
+  return <h1>I'm stuck =/ </h1>;
 };
 export default Profile;
