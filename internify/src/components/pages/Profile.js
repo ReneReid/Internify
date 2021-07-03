bcb8b1da7369647f3e202b7f2d115d7f43d88733
Aleem Tariq <aleemtariq19@gmail.com>
2021-07-03 00:14:14 -0700

    Added table information, more styling needed.

--- internify/src/components/pages/Profile.js
+++ internify/src/components/pages/Profile.js
@@ -1,15 +1,63 @@
 import "./styles/Profile.css";
 import { Grid } from "@material-ui/core";
 import { React, useState } from "react";
+import { TableBasic, TableStar } from "../molecules/Table";
 import angela from "../../assets/Profile/angela_brown.png";
 import RoomOutlinedIcon from "@material-ui/icons/RoomOutlined";
 import MailOutlineOutlinedIcon from "@material-ui/icons/MailOutlineOutlined";
 import PhoneAndroidOutlinedIcon from "@material-ui/icons/PhoneAndroidOutlined";
+import StarIcon from "@material-ui/icons/Star";
 
 const Profile = () => {
   const [profileDesc, setProfileDesc] = useState(
     "Angela is a university recruiter at Synch incorporated. Synch Inc. is a fast-paced security solutions tech startup founded in 2018. With a team of 30, we are looking to expand with young and fresh talent!"
   );
+
+  const [postingContent, setPostingContent] = useState([
+    {
+      title: "Software Developer",
+      dateCreated: "05/25/2021",
+      score: "Excellent",
+      link: "",
+    },
+    {
+      title: "Quality Assurance Developer",
+      dateCreated: "05/18/2021",
+      score: "Excellent",
+      link: "",
+    },
+    {
+      title: "Software Developer",
+      dateCreated: "04/30/2021",
+      score: "Good",
+      link: "",
+    },
+    {
+      title: "Test Developer",
+      dateCreated: "04/28/2021",
+      score: "Okay",
+      link: "",
+    },
+    {
+      title: "QA Analyst",
+      dateCreated: "04/28/2021",
+      score: "Okay",
+      link: "",
+    },
+    {
+      title: "Software Developer",
+      dateCreated: "04/22/2021",
+      score: "Excellent",
+      link: "",
+    },
+    {
+      title: "Full-Stack Developer",
+      dateCreated: "03/05/2021",
+      score: "Excellent",
+      link: "",
+    },
+  ]);
+
   return (
     <Grid
       container
@@ -135,7 +183,49 @@ const Profile = () => {
 
       {/* Right-hand grid item: Status,  */}
       <Grid item xs={7}>
-        <h3>Placeholder</h3>
+        {/* Heading container */}
+        <Grid
+          container
+          spacing={1}
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <div className="profile_right_heading">
+            <button>Edit profile</button>
+          </div>
+        </Grid>
+
+        {/* Recruiting status & postings container */}
+        <Grid
+          container
+          spacing={1}
+          direction="column"
+          justify="center"
+          alignItems="center"
+        >
+          {/* Recruiting status */}
+          <Grid item>
+            <div className="profile_left_status">
+              <ul className="profile_left_status_list">
+                <li>
+                  <h2>Status</h2>
+                </li>
+                <li>
+                  <StarIcon color="secondary" fontSize="inherit" />
+                  <b>Actively recruiting interns!</b> {"  "}
+                </li>
+              </ul>
+            </div>
+          </Grid>
+
+          {/* Posting status */}
+          <Grid item>
+            <div className="profile_left_posting_table">
+              <TableBasic className="posting_table" data={postingContent} />
+            </div>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
