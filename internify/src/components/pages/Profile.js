e9b172d430da27693055aedd4c55ec494d465815
Aleem Tariq <aleemtariq19@gmail.com>
2021-07-02 21:18:35 -0700

    Restyled according to Grid components.

--- internify/src/components/pages/Profile.js
+++ internify/src/components/pages/Profile.js
@@ -4,6 +4,43 @@ import { Grid } from "@material-ui/core";
 import React from "react";
 
 const Profile = () => {
-  return <h1>I'm stuck =/ </h1>;
+  return (
+    <Grid
+      container
+      spacing={1}
+      direction="row"
+      justify="center"
+      alignItems="center"
+    >
+      {/* Left-hand grid item: profile img, descsription, labels & contact */}
+      <Grid item xs={5}>
+        {/* Two containers, one for heading, another for content */}
+        <Grid
+          container
+          spacing={1}
+          direction="row"
+          justify="left"
+          alignItems="left"
+        >
+          <div className="profile_left_heading">
+            <h1> Your Profile </h1>
+          </div>
+        </Grid>
+
+        <Grid
+          container
+          spacing={1}
+          direction="column"
+          justify="center"
+          alignItems="center"
+        ></Grid>
+      </Grid>
+
+      {/* Right-hand grid item: Status,  */}
+      <Grid item xs={7}>
+        <h3>Placeholder</h3>
+      </Grid>
+    </Grid>
+  );
 };
 export default Profile;
