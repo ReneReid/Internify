425a3030b65a8968cae29a1b89c3d75469ba68dc
Aleem Tariq <aleemtariq19@gmail.com>
2021-07-02 21:58:32 -0700

    Added additional grid elements and styling.

--- internify/src/components/pages/Profile.js
+++ internify/src/components/pages/Profile.js
@@ -4,6 +4,9 @@ import { React, useState } from "react";
 import angela from "../../assets/Profile/angela_brown.png";
 
 const Profile = () => {
+  const [profileDesc, setProfileDesc] = useState(
+    "Angela is a university recruiter at Synch incorporated. Synch Inc. is a fast-paced security solutions tech startup founded in 2018. With a team of 30, we are looking to expand with young and fresh talent!"
+  );
   return (
     <Grid
       container
@@ -49,7 +52,59 @@ const Profile = () => {
                 <img src={angela} alt="Profile pic" className="profile_img" />
               </Grid>
               <Grid item>
-                <p> List description </p>
+                <ul className="profile_handle_desc">
+                  <li>
+                    <b> Angela Brown </b>
+                  </li>
+                  <li>
+                    <b> @angelab </b>
+                  </li>
+                  <li> Technical Recruiter @ Synch Inc </li>
+                </ul>
+              </Grid>
+            </Grid>
+
+            {/* Placeholder for future labels */}
+            <Grid
+              container
+              spacing={1}
+              direction="row"
+              justify="center"
+              alignItems="center"
+            ></Grid>
+
+            {/* Profile description */}
+            <Grid
+              container
+              spacing={1}
+              direction="row"
+              justify="center"
+              alignItems="center"
+            >
+              <Grid item>
+                <div className="profile_inner_desc">
+                  <p> {profileDesc} </p>
+                </div>
+              </Grid>
+            </Grid>
+
+            {/* Contact Information */}
+            <Grid
+              container
+              spacing={1}
+              direction="row"
+              justify="left"
+              alignItems="left"
+            >
+              <Grid item>
+                <div className="profile_inner_contact">
+                  <h3>Contact</h3>
+                  <ul className="profile_inner_contact_list">
+                    <li> Vancouver, BC </li>
+                    <li> angela@sync.com </li>
+                    <li> +1 699 (234) 1234 </li>
+                  </ul>
+                </div>
               </Grid>
             </Grid>
           </Grid>
