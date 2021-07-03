36f5d4aae4df0b00aea272460c027253084f0578
Aleem Tariq <aleemtariq19@gmail.com>
2021-07-02 21:42:19 -0700

    Added user image.

--- internify/src/components/pages/Profile.js
+++ internify/src/components/pages/Profile.js
@@ -1,7 +1,7 @@
 import "./styles/Profile.css";
 import { Grid } from "@material-ui/core";
-
-import React from "react";
+import { React, useState } from "react";
+import angela from "../../assets/Profile/angela_brown.png";
 
 const Profile = () => {
   return (
@@ -14,26 +14,46 @@ const Profile = () => {
     >
       {/* Left-hand grid item: profile img, descsription, labels & contact */}
       <Grid item xs={5}>
-        {/* Two containers, one for heading, another for content */}
+        {/* Heading container */}
         <Grid
           container
           spacing={1}
           direction="row"
-          justify="left"
-          alignItems="left"
+          justify="center"
+          alignItems="center"
         >
           <div className="profile_left_heading">
             <h1> Your Profile </h1>
           </div>
         </Grid>
 
+        {/* Content container */}
         <Grid
           container
           spacing={1}
           direction="column"
           justify="center"
           alignItems="center"
-        ></Grid>
+        >
+          {/* Content within a profile: image, list, labels, description, contact */}
+          <Grid item>
+            {/* Image and handle headers */}
+            <Grid
+              container
+              spacing={1}
+              direction="row"
+              justify="center"
+              alignItems="center"
+            >
+              <Grid item>
+                <img src={angela} alt="Profile pic" className="profile_img" />
+              </Grid>
+              <Grid item>
+                <p> List description </p>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
 
       {/* Right-hand grid item: Status,  */}
