5443fad091f2996707bc17c77ffb6203c100bfb3
Aleem Tariq <aleemtariq19@gmail.com>
2021-07-02 19:28:23 -0700

    Outline extended.

--- internify/src/components/pages/Profile.js
+++ internify/src/components/pages/Profile.js
@@ -5,19 +5,27 @@ import React from "react";
 
 const Profile = () => {
   return (
-    <Grid container direction="row" justify="center" alignItems="center">
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-      ></Grid>
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-      ></Grid>
+    <Grid container direction="column" justify="center" alignItems="center">
+      <Grid item>
+        <div className="header_tag_outer">
+          <div className="header_tag_inner_left">
+            <h2> Your Profile </h2>
+          </div>
+          <div className="header_tag_inner_right">
+            <button>Edit profile</button>
+          </div>
+        </div>
+      </Grid>
+
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item>
+          <h1> Hello </h1>
+        </Grid>
+        <Grid item>
+          <h1> World </h1>
+        </Grid>
+      </Grid>
+
       <Grid
         container
         direction="column"
