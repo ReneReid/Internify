c46f7e029003c96825b146e04f537e9b29384b59
Tommy Tho <tommythowm@gmail.com>
2021-07-05 19:43:37 -0700

    updated isGpaRequired field

--- internify/src/components/pages/Create.js
+++ internify/src/components/pages/Create.js
@@ -39,7 +39,7 @@ function Create(props) {
     },
     requirements: {
       experience: "",
-      gpa: "",
+      isGpaRequired: "",
       gpaValue: "",
       languages: [],
       frameworks: [],
