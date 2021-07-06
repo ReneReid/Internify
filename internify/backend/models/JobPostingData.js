cadf7ff9837e5d9aaedcb5a3cda05aebe20e5a8c
Aleem Tariq <aleemtariq19@gmail.com>
2021-07-06 12:55:20 -0700

    Updated JobPostingData in back-end model.

--- internify/backend/models/JobPostingData.js
+++ internify/backend/models/JobPostingData.js
@@ -1,76 +1,115 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
 const JobPostingSchema = new Schema({
+  header: {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     company: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
     },
     startDate: {
-        type: Date,
-        required: true,
-        default: Date.now
+      type: String,
+      required: true,
     },
-    positionLengthInMonths: {
-        type: Number,
-        required: true
+    position: {
+      type: [String],
+      required: true,
     },
-    positionType: {
-        type: [String],
-        required: true
+    length: {
+      type: String,
+      required: true,
     },
-    experienceLengthInMonths: {
-        type: Number,
-        required: true
+  },
+  requirements: {
+    experience: {
+      type: String,
+      required: true,
     },
     isGpaRequired: {
-        type: Boolean,
-        required: true
+      type: String,
+      required: true,
     },
-    gpaPercentage: Number,
-    codingLanguages: {
-        type: [String],
-        required: true
+    gpaValue: {
+      type: String,
+      required: true,
+    },
+    languages: {
+      type: [String],
+      required: true,
     },
     frameworks: {
-        type: [String],
-        required: true
+      type: [String],
+      required: true,
     },
-    workTools: {
-        type: [String],
-        required: true
+    tools: {
+      type: [String],
+      required: true,
     },
     concepts: {
-        type: [String],
-        required: true
+      type: [String],
+      required: true,
     },
-    jobDescription: {
-        type: String,
-        required: true
+  },
+  details: {
+    description: {
+      type: String,
+      required: true,
     },
-    locationType: {
-        type: String,
-        required: true
+    position: {
+      type: String,
+      required: true,
     },
-    salary: {
-        type: String,
-        required: true
+    pay: {
+      type: String,
+      required: true,
     },
-    citizenshipRequirements: {
-        type: String,
-        required: true
+    candidates: {
+      type: String,
+      required: true,
     },
-    academicRequirements: {
-        type: String,
-        required: true
+    academicReq: {
+      type: [String],
+      required: true,
     },
-    isCoop: Boolean
-
+    coOp: {
+      type: String,
+      required: true,
+    },
+  },
+  contact: {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    linkedIn: {
+      type: String,
+      required: false,
+    },
+    other: {
+      type: String,
+      required: false,
+    },
+    applicationSteps: {
+      type: String,
+      required: true,
+    },
+  },
 });
 
-module.exports = JobPostingData = mongoose.model('JobPostingData', JobPostingSchema);
+module.exports = JobPostingData = mongoose.model(
+  "JobPostingData",
+  JobPostingSchema
+);
