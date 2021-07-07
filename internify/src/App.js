7a39b62863efcf639d73e6e7916b2d7470fc1305
Tommy Tho <tommythowm@gmail.com>
2021-07-06 21:20:05 -0700

    added switch routing to authentication

--- internify/src/App.js
+++ internify/src/App.js
@@ -7,7 +7,12 @@ import Create from "./components/pages/Create";
 import Feedback from "./components/organisms/Feedback";
 import { EditModal } from "./components/molecules/index";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import firebase from "firebase/app";
 // If you enabled Analytics in your project, add the Firebase SDK for Analytics
@@ -27,35 +32,46 @@ function App() {
     storageBucket: "internify-8202d.appspot.com",
     messagingSenderId: "1050189046886",
     appId: "1:1050189046886:web:ff58ab36247021e6f4b9a7",
-    measurementId: "G-TJSRK7EK13"
+    measurementId: "G-TJSRK7EK13",
   };
 
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  let user = firebase.auth().onAuthStateChanged((user) => {
+    console.log(user);
+    return user;
+  });
 
   return (
     <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <Router>
-            <Switch>
-              <Route path="/edit">
-                <EditModal toggle={true} />
-              </Route>
-              <Route path="/login">
-                <Login />
-              </Route>
-              <Route path="/create">
-                <AuthNavbar />
-                <Create />
-              </Route>
-              <Route path="/feedback">
-                <Feedback />
-              </Route>
-              <Route path="/">
-                <Landing />
-              </Route>
-            </Switch>
+            {user ? (
+              <Switch>
+                <Route path="/edit">
+                  <EditModal toggle={true} />
+                </Route>
+                <Route path="/create">
+                  <AuthNavbar />
+                  <Create />
+                </Route>
+                <Route path="/feedback">
+                  <Feedback />
+                </Route>
+                <Redirect to="/create" />
+              </Switch>
+            ) : (
+              <Switch>
+                <Route path="/login">
+                  <Login />
+                </Route>
+                <Route path="/">
+                  <Landing />
+                </Route>
+                <Redirect to="/" />
+              </Switch>
+            )}
           </Router>
         </header>
       </div>
