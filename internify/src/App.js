a030fc5c43f24ae1d9fab7791276b537762c64b6
Tommy Tho <tommythowm@gmail.com>
2021-07-06 21:56:43 -0700

    fixed user update

--- internify/src/App.js
+++ internify/src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import AuthNavbar from "./components/organisms/AuthNavbar";
@@ -15,64 +16,78 @@ import {
 } from "react-router-dom";
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import firebase from "firebase/app";
-// If you enabled Analytics in your project, add the Firebase SDK for Analytics
-import "firebase/analytics";
-
 // Add the Firebase products that you want to use
 import "firebase/auth";
 import "firebase/firestore";
 
-function App() {
-  // TODO: Replace the following with your app's Firebase project configuration
-  // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-  const firebaseConfig = {
-    apiKey: "AIzaSyDXJD3MLG5x1gCLn1jZpS7v4mL7EnK4Lnc",
-    authDomain: "internify-8202d.firebaseapp.com",
-    projectId: "internify-8202d",
-    storageBucket: "internify-8202d.appspot.com",
-    messagingSenderId: "1050189046886",
-    appId: "1:1050189046886:web:ff58ab36247021e6f4b9a7",
-    measurementId: "G-TJSRK7EK13",
-  };
+// TODO: Replace the following with your app's Firebase project configuration
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+const firebaseConfig = {
+  apiKey: "AIzaSyDXJD3MLG5x1gCLn1jZpS7v4mL7EnK4Lnc",
+  authDomain: "internify-8202d.firebaseapp.com",
+  projectId: "internify-8202d",
+  storageBucket: "internify-8202d.appspot.com",
+  messagingSenderId: "1050189046886",
+  appId: "1:1050189046886:web:ff58ab36247021e6f4b9a7",
+  measurementId: "G-TJSRK7EK13",
+};
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  let user = firebase.auth().onAuthStateChanged((user) => {
-    console.log(user);
-    return user;
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) setUser(user);
+      setIsLoading(false);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   });
+
+  let routes;
+  if (isLoading) {
+    console.log("Loading");
+    routes = <div>Loading</div>;
+  }
+  if (user) {
+    console.log("Authenticated");
+    routes = (
+      <Switch>
+        <Route path="/edit">
+          <EditModal toggle={true} />
+        </Route>
+        <Route path="/create">
+          <AuthNavbar />
+          <Create />
+        </Route>
+        <Route path="/feedback">
+          <Feedback />
+        </Route>
+        <Redirect to="/create" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Landing />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  }
 
   return (
     <Provider store={store}>
       <div className="App">
         <header className="App-header">
-          <Router>
-            {user ? (
-              <Switch>
-                <Route path="/edit">
-                  <EditModal toggle={true} />
-                </Route>
-                <Route path="/create">
-                  <AuthNavbar />
-                  <Create />
-                </Route>
-                <Route path="/feedback">
-                  <Feedback />
-                </Route>
-                <Redirect to="/create" />
-              </Switch>
-            ) : (
-              <Switch>
-                <Route path="/login">
-                  <Login />
-                </Route>
-                <Route path="/">
-                  <Landing />
-                </Route>
-                <Redirect to="/" />
-              </Switch>
-            )}
-          </Router>
+          <Router>{routes}</Router>
         </header>
       </div>
     </Provider>
