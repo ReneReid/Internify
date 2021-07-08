54f09921f79d5de234b05c7778f43a4ed0b85d1d
Jude Shamsi <jude.shamsi@gmail.com>
2021-07-07 17:53:45 -0700

    updated API calls for /jobs to include findOne and findAll

--- internify/backend/routes/api/jobs.js
+++ internify/backend/routes/api/jobs.js
@@ -1,21 +1,30 @@
 var express = require('express');
 var router = express.Router();
-
-// Jobs Model //
 var JobPostingData = require('../../models/JobPostingData');
 
-//@route    GET api/jobs
-//@desc     Get All Job Posting Items
+//@route    GET api/jobs/getAll
+//@desc     Get All Job Documents
 //@access   Public
-router.get('/', function(req, res, next) {
+router.get('/getAll', function(req, res, next) {
   JobPostingData.find()
-    .then(jobs => res.json(jobs))
+    .then(jobs => res.status(200).json(jobs))
+    .catch(err => res.status(404).json({success: false}));
+});
+
+//@route    GET api/jobs/getOne/:id
+//@desc     Get Single Job Document
+//@access   Public
+router.get('/getOne/:id', function(req, res, next) {
+  JobPostingData.findById(req.params.id)
+    .then(jobs => res.status(200).json(jobs))
+    .catch(err => res.status(404).json({success: false}));
 });
 
 //@route    POST api/jobs
-//@desc     Create A Post 
+//@desc     Create A Job Document 
 //@access   Public
 router.post('/', function(req, res, next) {
+
     var newJob = new JobPostingData({
       title: req.body.title,
       company: req.body.company,
@@ -40,18 +49,18 @@ router.post('/', function(req, res, next) {
     newJob
       .save()
       .then(job => res.status(200).json(job))
-      .catch(err => console.log(err));
+      .catch(err => res.status(404).json({success: false}));
 
 });
 
-//@route    DELETE api/jobs
-//@desc     DELETE A Post 
+//@route    DELETE api/jobs/:id
+//@desc     DELETE A Job Document
 //@access   Public
 router.delete('/:id', function(req, res, next) {
     
   JobPostingData
     .findById(req.params.id)
-    .then(job => job.remove().then(() => res.json({success: true})))
+    .then(job => job.remove().then(() => res.status(200).json({success: true})))
     .catch(err => res.status(404).json({success: false}));
 });
 
