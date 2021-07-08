6dfd474ea18f78713a708af7c5ceeae34b0c3dff
Jude Shamsi <jude.shamsi@gmail.com>
2021-07-07 20:59:23 -0700

    updated jobs post api to use new schema

--- internify/backend/routes/api/jobs.js
+++ internify/backend/routes/api/jobs.js
@@ -5,7 +5,7 @@ var JobPostingData = require('../../models/JobPostingData');
 //@route    GET api/jobs/getAll
 //@desc     Get All Job Documents
 //@access   Public
-router.get('/getAll', function(req, res, next) {
+router.get('/', function(req, res, next) {
   JobPostingData.find()
     .then(jobs => res.status(200).json(jobs))
     .catch(err => res.status(404).json({success: false}));
@@ -24,25 +24,12 @@ router.get('/getOne/:id', function(req, res, next) {
 //@desc     Create A Job Document 
 //@access   Public
 router.post('/', function(req, res, next) {
-
+  console.log("in post: ", req.body);
     var newJob = new JobPostingData({
-      title: req.body.title,
-      company: req.body.company,
-      startDate: req.body.startDate,
-      positionLengthInMonths: req.body.positionLengthInMonths,
-      positionType: req.body.positionType,
-      experienceLengthInMonths: req.body.experienceLengthInMonths,
-      isGpaRequired: req.body.isGpaRequired,
-      gpaPercentage: req.body.gpaPercentage,
-      codingLanguages: req.body.codingLanguages,
-      frameworks: req.body.frameworks,
-      workTools: req.body.workTools,
-      concepts: req.body.concepts,
-      jobDescription: req.body.jobDescription,
-      locationType: req.body.locationType,
-      salary: req.body.salary,
-      citizenshipRequirements: req.body.citizenshipRequirements,
-      academicRequirements: req.body.academicRequirements,
+      header: req.body.header, 
+      requirements: req.body.requirements,
+      details: req.body.details,
+      contact: req.body.contact
     });
 
     // save new job to database
@@ -61,7 +48,10 @@ router.delete('/:id', function(req, res, next) {
   JobPostingData
     .findById(req.params.id)
     .then(job => job.remove().then(() => res.status(200).json({success: true})))
-    .catch(err => res.status(404).json({success: false}));
+    .catch(err => {
+      console.log(err);
+      res.status(404).json({success: false})
+    });
 });
 
 module.exports = router;
