d15c4bf65f1eda653adc5da4d67f731418de90fc
Tommy Tho <tommythowm@gmail.com>
2021-07-08 17:33:50 -0700

    updated imports from Create

---
+++ internify/src/components/pages/CreateJobPosting/index.js
@@ -0,0 +1,13 @@
+import CreateJobHeader from "./CreateJobHeader";
+import ContactDetails from "./ContactDetails";
+import JobDetail from "./JobDetail";
+import Review from "./Review";
+import TechRequirements from "./TechRequirements";
+
+export {
+    ContactDetails,
+    CreateJobHeader,
+    JobDetail,
+    Review,
+    TechRequirements
+};
