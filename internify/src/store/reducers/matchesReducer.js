875868b32afd644c18e02e01f9f6f67ab68ecd8c
Rene Reid <rdreid01@students.cs.ubc.ca>
2021-07-19 11:30:55 -0600

    just some (very very tentative) ideas for modifying the matches Reducer to facilitate better notes

--- internify/src/store/reducers/matchesReducer.js
+++ internify/src/store/reducers/matchesReducer.js
@@ -2,6 +2,7 @@ import { PROCESS_MATCHES } from "../actions/types/matchesTypes";
 
 const initialState = {
   matches: [],
+  
 };
 
 export default function matchesReducer(state = initialState, action) {
@@ -20,18 +21,30 @@ export default function matchesReducer(state = initialState, action) {
 // Inputs: students = [{student 1}, {student 2}, {student 3}, ...], posting = {a1, a2, ...}
 // Output: {postingID: {student 1, student 2, ...}}
 function matchFilter(students, posting) {
+
+  let gpaMatches = 0;
+  let coopMatches = 0;
+  let seekingMatches = 0;
+  let frameworkMatches = 0;
+  let workExpMatches = 0;
+  let langMatches = 0;
+  let toolsMatches = 0;
+  let conceptsMatches = 0;
+  let academicMatches = 0;
+  let citizenMatches = 0;
+
   let matchedStudents = students.filter(
     (student) =>
-      matchGpa(posting.gpa, posting.gpaValue, student.gpaValue) &&
-      matchCoop(student.coOp, posting.coOp) &&
-      matchSeek(student.seeking) &&
-      matchFrame(student.frameworks, posting.frameworks) &&
-      matchWork(student.experience, posting.experience) &&
-      matchLang(student.languages, posting.languages) &&
-      matchTools(student.tools, posting.tools) &&
-      matchConcepts(student.concepts, posting.concepts) &&
-      matchAcademicReq(student.academicReq, posting.academicReq) &&
-      matchCitizen(student.candidate, posting.candidates)
+      matchGpa(posting.gpa, posting.gpaValue, student.gpaValue, gpaMatches) &&
+      matchCoop(student.coOp, posting.coOp, coopMatches) &&
+      matchSeek(student.seeking, seekingMatches) &&
+      matchFrame(student.frameworks, posting.frameworks, frameworkMatches) &&
+      matchWork(student.experience, posting.experience, workExpMatches) &&
+      matchLang(student.languages, posting.languages, langMatches) &&
+      matchTools(student.tools, posting.tools, toolsMatches) &&
+      matchConcepts(student.concepts, posting.concepts, conceptsMatches) &&
+      matchAcademicReq(student.academicReq, posting.academicReq, academicMatches) &&
+      matchCitizen(student.candidate, posting.candidates, citizenMatches)
   );
 
   const jobId = posting.jobId;
@@ -42,95 +55,119 @@ function matchFilter(students, posting) {
 }
 
 // This section needs to be implemented
-function matchGpa(gpa, postingGpaValue, studentGpaValue) {
+function matchGpa(gpa, postingGpaValue, studentGpaValue, gpaMatches) {
   // empty case
   if (gpa === "") {
+    gpaMatches++;
     return true;
   }
 
   if (gpa === "Optional") {
+    gpaMatches++;
     return true;
   } else {
+    if (studentGpaValue >= postingGpaValue) {
+      gpaMatches++;
+    }
     return studentGpaValue >= postingGpaValue;
   }
 }
 
-function matchCoop(coOpStudent, coOpPosting) {
+function matchCoop(coOpStudent, coOpPosting, coopMatches) {
   // Co-op required or not
   if (coOpPosting) {
+    if (coOpStudent){
+      coopMatches++;
+    }
     return coOpStudent;
   } else {
+    coopMatches++;
     return true;
   }
 }
 
-function matchSeek(seeking) {
+function matchSeek(seeking, seekingMatches) {
+  if (seeking) {
+    seekingMatches++;
+  }
   return seeking;
 }
 
-function matchFrame(frameworksStudent, frameworksPosting) {
+function matchFrame(frameworksStudent, frameworksPosting, frameworkMatches) {
   // Empty case
   if (frameworksPosting.length === 0) {
+    frameworkMatches++;
     return true;
   }
 
   // Return true if student has at least one frame
   for (var i = 0; i < frameworksPosting.length; i++) {
     if (frameworksStudent[frameworksPosting[i]]) {
+      frameworkMatches++;
       return true;
     }
   }
   return false;
 }
 
-function matchWork(experienceStudent, experiencePosting) {
+function matchWork(experienceStudent, experiencePosting, workExpMatches) {
   // Null case
   if (isNaN(experiencePosting)) {
+    workExpMatches++;
     return true;
   } else {
     // Return true if student at least meets requirements
+    if (experienceStudent >= experiencePosting) {
+      workExpMatches++;
+    }
     return experienceStudent >= experiencePosting;
   }
 }
 
-function matchLang(languagesStudent, languagesPosting) {
+function matchLang(languagesStudent, languagesPosting, langMatches) {
   // Empty case
   if (languagesPosting.length === 0) {
+    langMatches++;
     return true;
   }
 
   // Return true if student has at least one language
   for (var i = 0; i < languagesPosting.length; i++) {
     if (languagesStudent[languagesPosting[i]]) {
+      langMatches++;
       return true;
     }
   }
   return false;
 }
 
-function matchTools(toolsStudent, toolsPosting) {
+function matchTools(toolsStudent, toolsPosting, toolsMatches) {
   // Empty case
   if (toolsPosting.length === 0) {
+    toolsMatches++;
     return true;
   }
 
   // Return true if student has at least one tool match
   for (var i = 0; i < toolsPosting.length; i++) {
     if (toolsStudent[toolsPosting[i]]) {
+      toolsMatches++;
       return true;
     }
   }
   return false;
 }
 
-function matchConcepts(conceptsStudent, conceptsPosting) {
+function matchConcepts(conceptsStudent, conceptsPosting, conceptsMatches) {
   // Empty case
   if (conceptsPosting.length === 0) {
+    conceptsMatches++;
     return true;
   }
   // Return true if student has at least one concept match
   for (var i = 0; i < conceptsPosting.length; i++) {
     if (conceptsStudent.includes(conceptsPosting[i])) {
+      conceptsMatches++;
       return true;
     }
   }
@@ -186,25 +223,34 @@ function studentDegreeRank(sDegrees) {
   return Math.max(...studentRanks);
 }
 
-function matchAcademicReq(academicReqStudent, academicReqPosting) {
+function matchAcademicReq(academicReqStudent, academicReqPosting, academicMatches) {
   // PhD > MSc > BSc > Associates > Diploma > Certificate (by rank)
   if (academicReqPosting.length === 0) {
+    academicMatches++;
     return true;
   }
   const studentRank = studentDegreeRank(academicReqStudent);
   const postingRank = postingDegreeRank(academicReqPosting);
+  if (studentRank >= postingRank) {
+    academicMatches++;
+  }
   return studentRank >= postingRank;
 }
 
-function matchCitizen(candidateStudent, candidatesPosting) {
+function matchCitizen(candidateStudent, candidatesPosting, citizenMatches) {
   // Empty case
   if (candidatesPosting.length === 3) {
+    citizenMatches++;
     return true;
   }
   // Anyone case
   if (candidatesPosting.includes("Anyone")) {
+    citizenMatches++;
     return true;
   } else {
+    if (candidatesPosting.includes(candidateStudent)) {
+      citizenMatches++;
+    }
     return candidatesPosting.includes(candidateStudent);
   }
 }
