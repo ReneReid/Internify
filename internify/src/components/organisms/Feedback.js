1cae418c76731bbe0b8b668005bbcb02674ab024
Rene Reid <rdreid01@students.cs.ubc.ca>
2021-07-22 21:18:56 -0600

    modifications made

--- internify/src/components/organisms/Feedback.js
+++ internify/src/components/organisms/Feedback.js
@@ -1,10 +1,10 @@
 import { React, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { ChipNote } from "../atoms/Chips";
-import Feedback1 from "../molecules/pages/Feedback1";
-import Feedback2 from "../molecules/pages/Feedback2";
-import Feedback3 from "../molecules/pages/Feedback3";
-import Feedback4 from "../molecules/pages/Feedback4";
+import TotalStudents from "../molecules/pages/Feedback1";
+import SeekingStudents from "../molecules/pages/Feedback2";
+import TechMatchStudents from "../molecules/pages/Feedback3";
+import FinalReqStudents from "../molecules/pages/Feedback4";
 import "./styles/Feedback.css";
 
 const Feedback = ({ page }) => {
@@ -16,44 +16,42 @@ const Feedback = ({ page }) => {
   if (page === 1) {
     return (
       <div className="feedback_outer">
-        <Feedback1 data={allStudents} />
+        <TotalStudents data={allStudents} />
       </div>
     );
   } else if (page === 2) {
-    const msg = `${pageObjects.page1Object.page1Students.length} total matches`;
     return (
       <div className="feedback_outer">
-        <Feedback1 data={allStudents} />
-        <Feedback2 data={pageObjects.page1Object} />
-        <ChipNote label={msg} />
+        <TotalStudents data={allStudents} />
+        <SeekingStudents data={pageObjects.page1Object} />
       </div>
     );
   } else if (page === 3) {
     const msg = `${pageObjects.page2Object.page2Students.length} total matches`;
     return (
       <div className="feedback_outer">
-        <Feedback1 data={allStudents} />
-        <Feedback2 data={pageObjects.page1Object} />
-        <Feedback3 data={pageObjects.page2Object} />
+        <TotalStudents data={allStudents} />
+        <SeekingStudents data={pageObjects.page1Object} />
+        <TechMatchStudents data={pageObjects.page2Object} />
         <ChipNote label={msg} />
       </div>
     );
   } else if (page === 4) {
     return (
       <div className="feedback_outer">
-        <Feedback1 data={allStudents} />
-        <Feedback2 data={pageObjects.page1Object} />
-        <Feedback3 data={pageObjects.page2Object} />
-        <Feedback4 data={pageObjects.page3Object} />
+        <TotalStudents data={allStudents} />
+        <SeekingStudents data={pageObjects.page1Object} />
+        <TechMatchStudents data={pageObjects.page2Object} />
+        <FinalReqStudents data={pageObjects.page3Object} />
       </div>
     );
   } else if (page === 5) {
     return (
       <div className="feedback_outer">
-        <Feedback1 data={allStudents} />
-        <Feedback2 data={pageObjects.page1Object} />
-        <Feedback3 data={pageObjects.page2Object} />
-        <Feedback4 data={pageObjects.page3Object} />
+        <TotalStudents data={allStudents} />
+        <SeekingStudents data={pageObjects.page1Object} />
+        <TechMatchStudents data={pageObjects.page2Object} />
+        <FinalReqStudents data={pageObjects.page3Object} />
       </div>
     );
   }
