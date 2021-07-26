13f803f33f7244627cf80bf5de67c29890369cac
Tommy Tho <tommythowm@gmail.com>
2021-07-25 19:51:01 -0700

    fixed warnings

--- internify/src/components/organisms/Feedback.js
+++ internify/src/components/organisms/Feedback.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react";
+import { React, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { ChipNote } from "../atoms/Chips";
 import TotalStudents from "../molecules/pages/TotalStudents";
