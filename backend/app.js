caa1975940b6f9e321a02fa3737cfe12154d35b8
Jude Shamsi <jude.shamsi@gmail.com>
2021-07-30 22:52:30 -0700

    updated static assets path

--- backend/app.js
+++ backend/app.js
@@ -39,10 +39,10 @@ app.use('/api/students', studentRouter);
 app.use('/api/users', userRouter);
 
 // Serve static assests if in production
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build'));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
