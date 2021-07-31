30d4a0fdc1ea1fcbe1218898c296508179f9c414
Jude Shamsi <jude.shamsi@gmail.com>
2021-07-30 23:18:51 -0700

    updating path to navigate to index.html

--- backend/app.js
+++ backend/app.js
@@ -40,10 +40,10 @@ app.use('/api/users', userRouter);
 
 // Serve static assests if in production
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/../client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, '/../client/build/index.html'));
   });
 }
 
