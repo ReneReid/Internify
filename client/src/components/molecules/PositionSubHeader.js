5507e0e806337797dac819c216a0ac1ee24d5055
Jude Shamsi <jude.shamsi@gmail.com>
2021-08-02 19:57:40 -0700

    updated position sub header to use original jobData object

--- client/src/components/molecules/PositionSubHeader.js
+++ client/src/components/molecules/PositionSubHeader.js
@@ -1,25 +1,20 @@
 import CheckBox from "../atoms/CheckBox";
 import React from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import { updateCheckBox } from "../../store/actions/jobPostActions";
 
 function PositionSubHeader(props) {
-  const data = props.jobData;
-  const checked = props.jobs.checkedBoxes;
+  const jobData = props.jobData;
 
   const handleChange = (e, v, l) => {
-    props.actions.updateCheckBox(e.target.name);
     filterChecked(e);
     props.updateKeysList(e, v, l);
   };
 
   function filterChecked(e){
     if (e.target.checked) {
-      data.position.push(e.target.name);
+      jobData.position.push(e.target.name);
     } else {
-      if (data.position.includes(e.target.name)) {
-        data.position = data.position.filter(
+      if (jobData.position.includes(e.target.name)) {
+        jobData.position = jobData.position.filter(
           (obj) => obj !== e.target.name
         );
       }
@@ -34,25 +29,25 @@ function PositionSubHeader(props) {
           name={"Internship"}
           label={"Internship"}
           onChange={(e) => handleChange(e, "position", "Internship")}
-          checked={checked.Internship}
+          checked={jobData.position.includes("Internship") ? true : false}
         />
         <CheckBox
           name={"Coop"}
           label={"Co-op"}
           onChange={(e) => handleChange(e, "position", "Co-op")}
-          checked={checked.Coop}
+          checked={jobData.position.includes("Coop") ? true : false}
         />
         <CheckBox
           name={"NewGrad"}
           label={"New Grad"}
           onChange={(e) => handleChange(e, "position", "New Grad")}
-          checked={checked.NewGrad}
+          checked={jobData.position.includes("NewGrad") ? true : false}
         />
         <CheckBox
           name={"PartTime"}
           label={"Part-time"}
           onChange={(e) => handleChange(e, "position", "Part-time")}
-          checked={checked.PartTime}
+          checked={jobData.position.includes("PartTime") ? true : false}
         />
       </div>
     </div>
@@ -60,21 +55,4 @@ function PositionSubHeader(props) {
 }
 
 
-function mapStateToProps(state) {
-  return {
-    jobs: state.jobs,
-  };
-}
-
-function matchDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(
-      {
-        updateCheckBox: updateCheckBox
-      },
-      dispatch
-    ),
-  };
-}
-
-export default connect(mapStateToProps, matchDispatchToProps)(PositionSubHeader);
+export default PositionSubHeader;
