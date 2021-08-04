0844078e20fffabfc7e57cf6590ec84bea6bb3f3
Tommy Tho <tommythowm@gmail.com>
2021-08-03 19:19:50 -0700

    reverted env typo

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ app.use('/api/users', userRouter);
 
 // Serve static assests if in production
 
-if(process.env.NODEgo_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/../client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/../client/build/index.html'));
