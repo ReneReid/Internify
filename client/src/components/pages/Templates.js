528208fc714af3b043081894d51c05bfe1307821
Rene Reid <rdreid01@students.cs.ubc.ca>
2021-08-05 17:45:23 -0600

    rudimentary display of each template object - needs reformatting

--- client/src/components/pages/Templates.js
+++ client/src/components/pages/Templates.js
@@ -1,12 +1,9 @@
-import { React, useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import { Container, Grid, Typography } from "@material-ui/core";
-import { ChevronLeft, CreateOutlined } from "@material-ui/icons";
-import { ButtonClear, ButtonOutlined } from "../atoms";
+import { React, } from "react";
+import { Link, } from "react-router-dom";
+import { Container, Grid} from "@material-ui/core";
+import { ChevronLeft, } from "@material-ui/icons";
+import { ButtonClear, } from "../atoms";
 import { ViewPosting } from "../molecules/index";
-import Alert from "@material-ui/lab/Alert";
-import LinkIcon from "@material-ui/icons/Link";
-import axios from "axios";
 import {
   backendStudent,
   frontendStudent,
@@ -16,28 +13,6 @@ import {
 import "./styles/View.css";
 
 const Templates = () => {
-  // let { slug } = useParams();
-  // const [job, setJob] = useState(null);
-  // const [copySuccess, setCopySuccess] = useState(false);
-
-  // useEffect(() => {
-  //   axios
-  //     .get(`/api/jobs/${slug}`)
-  //     .then((res) => setJob(res.data))
-  //     .catch((err) => console.error(err));
-  // }, [slug]);
-
-  // function copyToClipboard() {
-  //   navigator.clipboard
-  //     .writeText(window.location.href)
-  //     .then(() => {
-  //       setCopySuccess(true);
-  //       setTimeout(() => {
-  //         setCopySuccess(false);
-  //       }, 2000);
-  //     })
-  //     .catch((err) => console.error(err));
-  // }
 
   let templateList = [
     backendStudent,
@@ -46,84 +21,39 @@ const Templates = () => {
     fullstackStudent,
   ];
 
-  let job = templateList[0];
-
+  // let job = templateList[0];
   return (
-    <Grid
-      container
-      style={{ paddingTop: "1em" }}
-      direction="row"
-      alignItems="flex-start"
-      justifyContent="flex-end"
-    >
-      {/** Left side button */}
-      <Grid item xs={2}>
-        <Grid container justifyContent="flex-end">
-          <Link to="/profile">
-            <ButtonClear startIcon={<ChevronLeft />}>Back</ButtonClear>
-          </Link>
-        </Grid>
-      </Grid>
 
-      {/** Middle Job Posting */}
-      <Grid item xs={7}>
-        <Container maxWidth="md">
-          <ViewPosting data={job} />
-        </Container>
-      </Grid>
-
-      {/** Right side edditing -> matches as well? */}
-      {/* <Grid item xs={3}>
+    templateList.map((job) => {
+      return (
         <Grid
           container
-          direction="column"
+          style={{ paddingTop: "1em" }}
+          direction="row"
           alignItems="flex-start"
           justifyContent="flex-end"
         >
-          <div className="view_page_creation_details">
-            <div className="view_page_details_list">
-              <ul className="view_page_details_list">
-                <li key={"date-created"}>
-                  <b>Date created: </b>
-                  {job.dateCreated
-                    ? new Date(job.dateCreated).getDate() +
-                      "/" +
-                      (new Date(job.dateCreated).getMonth() + 1) +
-                      "/" +
-                      new Date(job.dateCreated).getFullYear()
-                    : null}
-                </li>
-                <li key={"job-posting-matches"}>
-                  Matches: <b>{job.matches}</b>
-                </li>
-              </ul>
-            </div>
-            <div className="view_page_buttons_list">
-              <ButtonOutlined styles={{}} startIcon={<CreateOutlined />}>
-                        Edit
-                      </ButtonOutlined>
-                      {"    "}
-                      <ButtonOutlined
-                        startIcon={<LinkIcon />}
-                        onClick={copyToClipboard}
-                      >
-                        Copy Link
-                      </ButtonOutlined>
-                      {copySuccess && (
-                        <Alert
-                          variant="outlined"
-                          severity="success"
-                          style={{ marginTop: "1em" }}
-                        >
-                          Successful copy to clipboard!
-                        </Alert>
-                      )}
-            </div>
-          </div>
+          {/** Left side button */}
+          <Grid item xs={2}>
+            <Grid container justifyContent="flex-end">
+              <Link to="/profile">
+                <ButtonClear startIcon={<ChevronLeft />}>Back</ButtonClear>
+              </Link>
+            </Grid>
+          </Grid>
+  
+          {/** Middle Job Posting */}
+          <Grid item xs={7}>
+            <Container maxWidth="md">
+              <ViewPosting data={job} />
+            </Container>
+          </Grid>
         </Grid>
-      </Grid> */}
-    </Grid>
-  );
+      );
+    })
+  )
+
+  
 };
 
 export default Templates;
