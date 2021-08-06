dfc7e2e9e7238cd7ebce5cb73d4432189e16d27e
Rene Reid <rdreid01@students.cs.ubc.ca>
2021-08-06 10:47:28 -0600

    further stylistic changes made to align with Figma

--- client/src/components/pages/Templates.js
+++ client/src/components/pages/Templates.js
@@ -13,28 +13,12 @@ import {
 import "./styles/View.css";
 
 const Templates = () => {
-
   function dummyfunction() {
     console.log("Hello");
   }
 
   return (
-
     <Grid>
-      <div className="page_title">
-      <h1 >Select Prefills</h1>
-      <h4>Select from 4 available templates</h4>
-      </div>
-      
-
-    <Grid
-      container
-      style={{ paddingTop: "1em" }}
-      direction="row"
-      alignItems="flex-start"
-      justifyContent="flex-end"
-    >
-      {/** Left side button */}
       <Grid item xs={2}>
         <Grid container justifyContent="flex-end">
           <Link to="/selection">
@@ -42,30 +26,50 @@ const Templates = () => {
           </Link>
         </Grid>
       </Grid>
-
-      {/** Right-hand templates */}
       <Grid item xs={10}>
-        <Grid container direction="row">
-          <div className="templates_container">
-            <div class="template" onClick={() => dummyfunction()}>
-              <ViewPosting data={backEndStudent} hide={true}/>
+        <div className="page_title">
+          <h1>Select Prefills</h1>
+          <h4 className="message">Select from 4 available templates</h4>
+        </div>
+      </Grid>
+      <Grid
+        container
+        style={{ paddingTop: "1em" }}
+        direction="row"
+        alignItems="flex-start"
+        justifyContent="flex-end"
+      >
+        {/** Left side button */}
+        {/* <Grid item xs={2}>
+          <Grid container justifyContent="flex-end">
+            <Link to="/selection">
+              <ButtonClear startIcon={<ChevronLeft />}>Back</ButtonClear>
+            </Link>
+          </Grid>
+        </Grid> */}
+
+        {/** Right-hand templates */}
+        <Grid item xs={10}>
+          <Grid container direction="row">
+            <div className="templates_container">
+              <div class="template" onClick={() => dummyfunction()}>
+                <ViewPosting data={backEndStudent} hide={true} />
+              </div>
+              <div class="template" onClick={() => dummyfunction()}>
+                <ViewPosting data={frontEndStudent} hide={true} />
+              </div>
+              <div class="template" onClick={() => dummyfunction()}>
+                <ViewPosting data={dataScienceStudent} hide={true} />
+              </div>
+              <div class="template" onClick={() => dummyfunction()}>
+                <ViewPosting data={fullStackStudent} hide={true} />
+              </div>
             </div>
-            <div class="template">
-              <ViewPosting data={frontEndStudent} hide={true}/>
-            </div>
-            <div class="template">
-              <ViewPosting data={dataScienceStudent} hide={true}/>
-            </div>
-            <div class="template">
-              <ViewPosting data={fullStackStudent} hide={true}/>
-            </div>
-          </div>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
-    </Grid>
   );
-
 };
 
 export default Templates;
