523f541ecb80b075ea1e1dda49bc34b71ccd5798
Aleem Tariq <aleemtariq19@gmail.com>
2021-08-05 17:23:38 -0700

    Made the templates horizontal with scrollbar

--- client/src/components/pages/Templates.js
+++ client/src/components/pages/Templates.js
@@ -1,8 +1,8 @@
-import { React, } from "react";
-import { Link, } from "react-router-dom";
-import { Container, Grid} from "@material-ui/core";
-import { ChevronLeft, } from "@material-ui/icons";
-import { ButtonClear, } from "../atoms";
+import { React } from "react";
+import { Link } from "react-router-dom";
+import { Container, Grid } from "@material-ui/core";
+import { ChevronLeft } from "@material-ui/icons";
+import { ButtonClear } from "../atoms";
 import { ViewPosting } from "../molecules/index";
 import {
   backendStudent,
@@ -13,7 +13,6 @@ import {
 import "./styles/View.css";
 
 const Templates = () => {
-
   let templateList = [
     backendStudent,
     frontendStudent,
@@ -21,39 +20,79 @@ const Templates = () => {
     fullstackStudent,
   ];
 
-  // let job = templateList[0];
+  function dummyfunction() {
+    console.log("Hello");
+  }
+
   return (
-
-    templateList.map((job) => {
-      return (
-        <Grid
-          container
-          style={{ paddingTop: "1em" }}
-          direction="row"
-          alignItems="flex-start"
-          justifyContent="flex-end"
-        >
-          {/** Left side button */}
-          <Grid item xs={2}>
-            <Grid container justifyContent="flex-end">
-              <Link to="/profile">
-                <ButtonClear startIcon={<ChevronLeft />}>Back</ButtonClear>
-              </Link>
-            </Grid>
-          </Grid>
-  
-          {/** Middle Job Posting */}
-          <Grid item xs={7}>
-            <Container maxWidth="md">
-              <ViewPosting data={job} />
-            </Container>
-          </Grid>
+    <Grid
+      container
+      style={{ paddingTop: "1em" }}
+      direction="row"
+      alignItems="flex-start"
+      justifyContent="flex-end"
+    >
+      {/** Left side button */}
+      <Grid item xs={2}>
+        <Grid container justifyContent="flex-end">
+          <Link to="/profile">
+            <ButtonClear startIcon={<ChevronLeft />}>Back</ButtonClear>
+          </Link>
         </Grid>
-      );
-    })
-  )
+      </Grid>
 
-  
+      {/** Right-hand templates */}
+      <Grid item xs={10}>
+        <Grid container direction="row">
+          <div className="templates_container">
+            <div class="template" onClick={() => dummyfunction()}>
+              <ViewPosting data={backendStudent} />
+            </div>
+            <div class="template">
+              <ViewPosting data={frontendStudent} />
+            </div>
+            <div class="template">
+              <ViewPosting data={datascienceStudent} />
+            </div>
+            <div class="template">
+              <ViewPosting data={fullstackStudent} />
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+
+  // // let job = templateList[0];
+  // return (
+  //   templateList.map((job) => {
+  //     return (
+  //       <Grid
+  //         container
+  //         style={{ paddingTop: "1em" }}
+  //         direction="row"
+  //         alignItems="flex-start"
+  //         justifyContent="flex-end"
+  //       >
+  //         {/** Left side button */}
+  //         <Grid item xs={2}>
+  //           <Grid container justifyContent="flex-end">
+  //             <Link to="/profile">
+  //               <ButtonClear startIcon={<ChevronLeft />}>Back</ButtonClear>
+  //             </Link>
+  //           </Grid>
+  //         </Grid>
+
+  //         {/** Middle Job Posting */}
+  //         <Grid item xs={7}>
+  //           <Container maxWidth="md">
+  //             <ViewPosting data={job} />
+  //           </Container>
+  //         </Grid>
+  //       </Grid>
+  //     );
+  //   })
+  // )
 };
 
 export default Templates;
