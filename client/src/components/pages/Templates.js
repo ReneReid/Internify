dd4b573a6bc80877139de107aec3ac51af0b7049
Rene Reid <rdreid01@students.cs.ubc.ca>
2021-08-05 18:27:17 -0600

    commented out and unused code removed

--- client/src/components/pages/Templates.js
+++ client/src/components/pages/Templates.js
@@ -1,6 +1,6 @@
 import { React } from "react";
 import { Link } from "react-router-dom";
-import { Container, Grid } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import { ChevronLeft } from "@material-ui/icons";
 import { ButtonClear } from "../atoms";
 import { ViewPosting } from "../molecules/index";
@@ -13,12 +13,6 @@ import {
 import "./styles/View.css";
 
 const Templates = () => {
-  let templateList = [
-    backendStudent,
-    frontendStudent,
-    datascienceStudent,
-    fullstackStudent,
-  ];
 
   function dummyfunction() {
     console.log("Hello");
@@ -63,36 +57,6 @@ const Templates = () => {
     </Grid>
   );
 
-  // // let job = templateList[0];
-  // return (
-  //   templateList.map((job) => {
-  //     return (
-  //       <Grid
-  //         container
-  //         style={{ paddingTop: "1em" }}
-  //         direction="row"
-  //         alignItems="flex-start"
-  //         justifyContent="flex-end"
-  //       >
-  //         {/** Left side button */}
-  //         <Grid item xs={2}>
-  //           <Grid container justifyContent="flex-end">
-  //             <Link to="/profile">
-  //               <ButtonClear startIcon={<ChevronLeft />}>Back</ButtonClear>
-  //             </Link>
-  //           </Grid>
-  //         </Grid>
-
-  //         {/** Middle Job Posting */}
-  //         <Grid item xs={7}>
-  //           <Container maxWidth="md">
-  //             <ViewPosting data={job} />
-  //           </Container>
-  //         </Grid>
-  //       </Grid>
-  //     );
-  //   })
-  // )
 };
 
 export default Templates;
