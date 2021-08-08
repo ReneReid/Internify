2c24fc4cac373c49a065a08c414d7ef591a30d45
Rene Reid <rdreid01@students.cs.ubc.ca>
2021-08-08 17:52:24 -0600

    console logs deleted

--- client/src/store/reducers/userReducer.js
+++ client/src/store/reducers/userReducer.js
@@ -31,10 +31,8 @@ export default function userReducer(state = initialState, action) {
 
     case UPDATE_JOBS_OF_USER:
       const newJobs = action.payload.jobPostings;
-      console.log(newJobs);
       state.user.jobPostings?.push(newJobs[0]);
       let uniqJobs = (arr) => arr.filter((v, i) => arr.indexOf(v) === i);
-      console.log(uniqJobs(state.user.jobPostings));
       state.user.jobPostings = uniqJobs(state.user.jobPostings);
       return {...state, user: state.user};
 
